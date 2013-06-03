4d3fb0637f031f8eef8379c558e8e7023bd95cb6
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-06-03 11:24:16 +0530

    Better Installations

--- install.sh
+++ install.sh
@@ -12,6 +12,11 @@ then
 fi
 
 
+# Make Variables Available For Later Use
+LOGDIR=/var/log/easyengine
+INSTALLLOG=/var/log/easyengine/install.log
+
+
 # Capture Errors
 OwnError()
 {
@@ -19,29 +24,22 @@ OwnError()
 	exit 101
 }
 
-
-# Make Variables Available For Later Use
-LOGDIR=/var/log/easyengine
-INSTALLLOG=/var/log/easyengine/install.log
-
-
-
 # Pre Checks To Avoid Later Screw Ups
 # Checking Logs Directory
 echo &>> $INSTALLLOG
 echo &>> $INSTALLLOG
-echo -e "\033[34m EasyEngine Installation Started `date +%d-%b-%Y:%H:%M:%S` \e[0m" | tee -ai $INSTALLLOG
+echo -e "\033[34m EasyEngine Installation Started `date +"%d-%b-%Y %H:%M:%S"` \e[0m" | tee -ai $INSTALLLOG
 
 if [ ! -d $LOGDIR ]
 then
-	echo -e "\033[34m Creating Easy Engine Log Directory...  \e[0m" | tee -ai $INSTALLLOG
+	echo -e "\033[34m Creating Easy Engine Log Directory, Please Wait...  \e[0m" | tee -ai $INSTALLLOG
 	mkdir -p $LOGDIR || OwnError "Unable To Create Log Directory $LOGDIR"
 fi
 
 # Checking Tee
 if [ ! -x  /usr/bin/tee ]
 then
-	echo -e "\033[31m Tee Command Not Found !! \e[0m" | tee -ai $INSTALLLOG
+	echo -e "\033[31m Tee Command Not Found ! \e[0m" | tee -ai $INSTALLLOG
 	echo -e "\033[34m Installing Tee  \e[0m" | tee -ai $INSTALLLOG
 	sudo apt-get -y install coreutils || OwnError "Unable to install tee"
 fi
@@ -49,7 +47,7 @@ fi
 # Checking Wget
 if [ ! -x  /usr/bin/wget ]
 then
-	echo -e "\033[31m Wget Command Not Found !! \e[0m" | tee -ai $INSTALLLOG
+	echo -e "\033[31m Wget Command Not Found ! \e[0m" | tee -ai $INSTALLLOG
 	echo -e "\033[34m Installing Wget  \e[0m" | tee -ai $INSTALLLOG
 	sudo apt-get -y install wget || OwnError "Unable To Install Wget"
 fi
@@ -57,7 +55,7 @@ fi
 # Checking Tar
 if [ ! -x  /bin/tar ]
 then
-	echo -e "\033[31m Tar Command Not Found !! \e[0m" | tee -ai $INSTALLLOG
+	echo -e "\033[31m Tar Command Not Found ! \e[0m" | tee -ai $INSTALLLOG
 	echo -e "\033[34m Installing Tar  \e[0m" | tee -ai $INSTALLLOG
 	sudo apt-get -y install tar || OwnError "Unable To Install Tar"
 fi
@@ -65,7 +63,7 @@ fi
 # Checking Name Servers
 if [[ -z $(cat /etc/resolv.conf | grep -v ^#) ]]
 then
-	echo -e "\033[31m No Name Servers Detected !! \e[0m" | tee -ai $INSTALLLOG
+	echo -e "\033[31m No Name Servers Detected ! \e[0m" | tee -ai $INSTALLLOG
 	echo -e "\033[31m Please Configure /etc/resolv.conf \e[0m" | tee -ai $INSTALLLOG
 	exit 102
 fi
@@ -73,19 +71,19 @@ fi
 # Checking Git
 if [ ! -x  /usr/bin/git ]
 then
-	echo -e "\033[31m Git Command Not Found !! \e[0m" | tee -ai $INSTALLLOG
+	echo -e "\033[31m Git Command Not Found ! \e[0m" | tee -ai $INSTALLLOG
 	echo -e "\033[34m Installing Git, Please Wait...  \e[0m" | tee -ai $INSTALLLOG
 	sudo apt-get -y install git-core || OwnError "Unable To Install Git"
 fi
 
 # Checking WP-CLI
-if [ ! -d /root/wp-cli ]
-then
-	echo -e "\033[31m WP Command Not Found !! \e[0m"
-	echo -e "\033[34m Installing WP-CLI, Please Wait...  \e[0m"
-	git clone git://github.com/wp-cli/wp-cli.git /root/wp-cli
-	sudo /root/wp-cli/utils/dev-build || OwnError "Unable To Build WP-CLI"
-fi
+#if [ ! -d /root/wp-cli ]
+#then
+#	echo -e "\033[31m WP Command Not Found ! \e[0m"
+#	echo -e "\033[34m Installing WP-CLI, Please Wait...  \e[0m"
+#	git clone git://github.com/wp-cli/wp-cli.git /root/wp-cli
+#	sudo /root/wp-cli/utils/dev-build || OwnError "Unable To Build WP-CLI"
+#fi
 
 # Pre Checks End
 
@@ -95,19 +93,22 @@ EXIST=$(basename `pwd`)
 if [ "$EXIST" != "easyengine" ]
 then
 	echo -e "\033[34m Cloning Easy Engine, Please Wait...  \e[0m" | tee -ai $INSTALLLOG
-	cd /tmp
-
+	
 	# Remove Older Easy Engine If Found
+	cd /tmp
 	rm -rf /tmp/easyengine &>> /dev/null
 
 	# Git Clone
 	git clone git://github.com/rtCamp/easyengine.git || OwnError "Unable To Clone Easy Engine"
+
+	# Change Directory
 	cd easyengine
 fi
 
 # Create Directory /usr/share/easyengine
 if [ ! -d /usr/share/easyengine ]
 then
+	# Create A Directory For EE Configurations
 	mkdir -p /usr/share/easyengine \
 	|| OwnError "Unable To Create Dir /usr/share/easyengine"
 fi
