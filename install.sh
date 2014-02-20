11ccabc5841e08deb985e498f1a09b3baec8dab2
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2014-02-20 13:09:44 +0530

    Fixed size 50MB for fastcgi.conf

--- install.sh
+++ install.sh
@@ -158,11 +158,11 @@ then
 fi
 
 # Adjust FastCGI Cache Size 20% Of /var/run
-VARRUNSIZE=$(df --block-size=M /var/run | awk '{print $4}' | tail -n1 |cut -d'M' -f1)
-FCSIZE=$(expr $VARRUNSIZE \* 25 / 100)
+#VARRUNSIZE=$(df --block-size=M /var/run | awk '{print $4}' | tail -n1 |cut -d'M' -f1)
+#FCSIZE=$(expr $VARRUNSIZE \* 25 / 100)
 
 # Change Size
-sed -i "s/500m/$FCSIZE\m/" /usr/share/easyengine/nginx/conf.d/fastcgi.conf || OwnError "Unable To Change Fastcgi Cache Size"
+#sed -i "s/500m/$FCSIZE\m/" /usr/share/easyengine/nginx/conf.d/fastcgi.conf || OwnError "Unable To Change Fastcgi Cache Size"
 
 # Git Config Settings
 EEGITNAME=$(git config user.name)
