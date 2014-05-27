9a61dbe9ba17c17eec40fe3c89a3d978e91fab33
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-05-27 14:52:57 +0530

    Minor update

--- src/lib/apt_get_update.sh
+++ src/lib/apt_get_update.sh
@@ -2,6 +2,6 @@
 
 function apt_get_update()
 {
-	echo_blue "apt-get update, Please Wait..."
+	echo_blue "Executing apt-get update, Please Wait..."
 	apt-get update &>> $EE_LOG || ee_error "Unable to execute apt-get update, exit status = " $?
 }
--- src/lib/dot_deb_gpg_key.sh
+++ src/lib/dot_deb_gpg_key.sh
@@ -2,6 +2,8 @@
 
 function dot_deb_gpg_key()
 {
-	wget --no-check-certificate -cqO /tmp/dotdeb.gpg http://www.dotdeb.org/dotdeb.gpg || ee_error "Unable to download dotdeb GnuPG key, exit status = " $?
-	apt-key add /tmp/dotdeb.gpg &>> $EE_LOG || ee_error "Unable to add dotdeb GnuPG key, exit status = " $?
+	wget --no-check-certificate -cqO /tmp/dotdeb.gpg http://www.dotdeb.org/dotdeb.gpg \
+	|| ee_error "Unable to download dotdeb GnuPG key, exit status = " $?
+	apt-key add /tmp/dotdeb.gpg &>> $EE_LOG \
+	|| ee_error "Unable to add dotdeb GnuPG key, exit status = " $?
 }
