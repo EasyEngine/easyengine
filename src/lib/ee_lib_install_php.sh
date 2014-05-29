cbc31c2c1d8c351d8b80507f3b71f56066465ac5
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-05-29 18:09:07 +0530

    Setup php5-fpm

---
+++ src/lib/ee_lib_install_php.sh
@@ -0,0 +1,9 @@
+# Install php5-fpm package
+
+function ee_lib_install_php()
+{
+	ee_lib_echo "Installing php5, please wait..."
+	$EE_APT_GET install php5-common php5-mysqlnd php5-xmlrpc \
+	php5-curl php5-gd php5-cli php5-fpm php5-imap php5-mcrypt php5-xdebug \
+	php5-memcache memcached || ee_lib_error "Unable to install php5, exit status = " $?
+}
