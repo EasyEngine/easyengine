4be2ab3c506e8b3b30b67dcde7e544ed020b2f17
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-06-05 17:30:00 +0530

    Minor update

--- src/modules/system/ee_system_status.sh
+++ src/modules/system/ee_system_status.sh
@@ -2,24 +2,34 @@
 
 function ee_system_status()
 {
+	# Detect operating system
 	local ee_operating_system=$(lsb_release -d | awk '{print $2,$3,$4}')
+
+	# Detect system load and processes
 	local ee_system_load=$(cat /proc/loadavg | awk '{print $1}')
 	local ee_system_processes=$(ps ax | wc -l)
+
+	# Uses of / partition and users logged in
+	local ee_logged_in_users=$(w -h | wc -l)
+	local ee_root_usage=$(df -h | grep /$ | head -1 | awk '{print $5}')
+
+	# Memory uses
 	local ee_memory_total=$(free | grep Mem: | awk '{print $2}')
 	local ee_memory_used=$(free | grep Mem: | awk '{print $3}')
 	local ee_memory_buffers=$(free | grep Mem: | awk '{print $6}')
 	local ee_memory_cache=$(free | grep Mem: | awk '{print $7}')
-	local ee_swap_total=$(free | grep Swap: | awk '{print $2}')
 	local ee_memory_usage=$(echo "($ee_memory_used-$ee_memory_buffers-$ee_memory_cache)*100/$ee_memory_total" | bc -l | cut -d'.' -f1)
+
+	# Swap uses
+	local ee_swap_total=$(free | grep Swap: | awk '{print $2}')
 	if [[ $ee_swap_total > 0 ]]; then
 		local ee_swap_used=$(free | grep Swap: | awk '{print $3}')
 		local ee_swap_usage=$(echo "$ee_swap_used*100/$ee_swap_total" | bc -l | cut -d'.' -f1)
 	else
 		local ee_swap_usage=$(echo "N/A")
 	fi
-	local ee_logged_in_users=$(w -h | wc -l)
-	local ee_root_usage=$(df -h | grep /$ | head -1 | awk '{print $5}')
 
+	# Service status
 	local ee_nginx_status=$(service nginx status | grep 'nginx is running' \
 	&>> $EE_COMMAND_LOG && ee_lib_echo "Running" || ee_lib_echo_fail "Stopped")
 	local ee_php_status=$(service php5-fpm status | grep running \
