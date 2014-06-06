135e631657cbd599531ab4f7565da55e16405dfe
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-06-06 14:43:40 +0530

    Remove unwanted messages

--- src/modules/system/install/ee_mod_setup_nginx.sh
+++ src/modules/system/install/ee_mod_setup_nginx.sh
@@ -43,7 +43,7 @@ function ee_mod_setup_nginx()
 	fi
 
 	# Copy files
-	cp -av /usr/share/easyengine/nginx/conf.d /usr/share/easyengine/nginx/common /etc/nginx
+	cp -a /usr/share/easyengine/nginx/conf.d /usr/share/easyengine/nginx/common /etc/nginx
 
 	# Setup port 22222
 	cp -v /usr/share/easyengine/nginx/22222 /etc/nginx/sites-available/
