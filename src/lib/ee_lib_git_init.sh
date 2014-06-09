eed386af92734a7173601ba799640d26a457e7c3
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-06-09 14:52:25 +0530

    ee stack install

--- src/lib/ee_lib_git_init.sh
+++ src/lib/ee_lib_git_init.sh
@@ -2,20 +2,22 @@
 
 function ee_lib_git_init()
 {
-	# Change directory
-	cd $EE_GIT_DIR || ee_lib_error "Unable to change directory $EE_GIT_DIR, exit status = " $?
+	for ee_git_dir in $@; do
+		# Change directory
+		cd $ee_git_dir || ee_lib_error "Unable to change directory $ee_git_dir, exit status = " $?
 
-	# Check .git
-	if [ ! -d .git ]; then
-		ee_lib_echo "Initialize Git On $EE_GIT_DIR..."
-		git init &>> $EE_COMMAND_LOG \
-		|| ee_lib_error "Unable to initialize Git on $EE_GIT_DIR, exit status = " $?
-	fi
+		# Check .git
+		if [ ! -d .git ]; then
+			ee_lib_echo "Initialize Git On ${ee_git_dir}..."
+			git init &>> $EE_COMMAND_LOG \
+			|| ee_lib_error "Unable to initialize Git on $ee_git_dir, exit status = " $?
+		fi
 
-	# Check for untracked files
-	if [ $(git status -s | wc -l) -ne 0 ]; then
-		# Add files in Git version control
-		git add --all && git commit -am "Initialize Git On $EE_GIT_DIR"  &>> $EE_COMMAND_LOG \
-		|| ee_lib_error "Unable to Git commit on $EE_GIT_DIR, exit status = " $?
-	fi
+		# Check for untracked files
+		if [ $(git status -s | wc -l) -ne 0 ]; then
+			# Add files in Git version control
+			git add --all && git commit -am "Initialize Git On $ee_git_dir"  &>> $EE_COMMAND_LOG \
+			|| ee_lib_error "Unable to Git commit on $ee_git_dir, exit status = " $?
+		fi
+	done
 }
