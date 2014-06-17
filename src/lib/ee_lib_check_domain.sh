939e14497a82c0ed54b9fe39790fc7f5c7e94135
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-06-17 16:36:36 +0530

    Minor changes

--- src/lib/ee_lib_check_domain.sh
+++ src/lib/ee_lib_check_domain.sh
@@ -3,14 +3,14 @@
 function ee_lib_check_domain()
 {
 	# Check if domain name is empty or not
-	while [ -z $EE_DOMAIN ]; do
+	while [ -z $EE_DOMAIN_CHECK ]; do
 		# Ask user to enter domain name
-		read -p "Enter domain name: " EE_DOMAIN
+		read -p "Enter domain name: " EE_DOMAIN_CHECK
 	done
 
 	# Remove http://  https:// & www.
-	EE_DOMAIN=$(echo $EE_DOMAIN | tr 'A-Z' 'a-z' |  sed "s'http://''" | sed "s'https://''" | sed "s'www.''" | sed "s'/''")
+	EE_DOMAIN=$(echo $EE_DOMAIN_CHECK | tr 'A-Z' 'a-z' |  sed "s'http://''" | sed "s'https://''" | sed "s'/''" | sed "s'www.''" )
 	
 	# Remove http:// https:// For WordPress Setup (www.example.com)
-	EE_WWW_DOMAIN=$(echo $EE_DOMAIN | tr 'A-Z' 'a-z' | sed "s'http://''" | sed "s'https://''" | sed "s'/''")
+	EE_WWW_DOMAIN=$(echo $EE_DOMAIN_CHECK | tr 'A-Z' 'a-z' | sed "s'http://''" | sed "s'https://''" | sed "s'/''")
 }
