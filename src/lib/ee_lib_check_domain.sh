e00ee53910dd266028bf5bc303f7be45c6e6a90b
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-06-17 11:08:49 +0530

    Better code to check domain name

--- src/lib/ee_lib_check_domain.sh
+++ src/lib/ee_lib_check_domain.sh
@@ -2,7 +2,7 @@
 
 function ee_lib_check_domain()
 {
-	# Check if domain name empty
+	# Check if domain name is empty or not
 	while [ -z $1 ]; do
 		# Ask user to enter domain name
 		read -p "Enter domain name: " EE_DOMAIN
@@ -14,4 +14,3 @@ function ee_lib_check_domain()
 	# Remove http:// https:// For WordPress Setup (www.example.com)
 	EE_WWW_DOMAIN=$(echo $EE_DOMAIN | tr 'A-Z' 'a-z' | sed "s'http://''" | sed "s'https://''" | sed "s'/''")
 }
-
