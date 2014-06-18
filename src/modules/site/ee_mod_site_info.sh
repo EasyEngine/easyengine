2d8de7606f397e2a4c15e40aababda6458e02272
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-06-18 15:46:24 +0530

    Fixed ee site info command

--- src/modules/site/ee_mod_site_info.sh
+++ src/modules/site/ee_mod_site_info.sh
@@ -7,9 +7,9 @@ function ee_mod_site_info()
 	local ee_access_log=$(grep access_log /etc/nginx/sites-available/$EE_DOMAIN | grep "/var/log/nginx/" | awk '{print($2)}' | cut -d ';' -f1)
 	local ee_error_log=$(grep error_log /etc/nginx/sites-available/$EE_DOMAIN | grep "/var/log/nginx/" | awk '{print($2)}' | cut -d ';' -f1)
 	local ee_webroot=$(grep root /etc/nginx/sites-available/$EE_DOMAIN | grep htdocs | awk '{print($2)}'  | cut -d ';' -f1)
-	local ee_db_name=$(grep DB_NAME $EE_WP_CONFIG_PATH 2> /dev/null | cut -d"'" -f4)
-	local ee_db_user=$(grep DB_USER $EE_WP_CONFIG_PATH 2> /dev/null | cut -d"'" -f4)
-	local ee_db_pass=$(grep DB_PASS $EE_WP_CONFIG_PATH 2> /dev/null | cut -d"'" -f4)
+	local ee_db_name=$(grep DB_NAME /var/www/$EE_DOMAIN/*-config.php 2> /dev/null | cut -d"'" -f4)
+	local ee_db_user=$(grep DB_USER /var/www/$EE_DOMAIN/*-config.php 2> /dev/null | cut -d"'" -f4)
+	local ee_db_pass=$(grep DB_PASS /var/www/$EE_DOMAIN/*-config.php 2> /dev/null | cut -d"'" -f4)
 
 	ee_lib_echo "Information about $EE_DOMAIN:"
 	ee_lib_echo_escape "\nNginx configuration\t \033[37m$ee_site_info ($ee_site_status)"
