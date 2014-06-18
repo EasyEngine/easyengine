a4e77db88e632ff9f2f3fee5c9a7096b83e2fd51
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-06-18 15:40:35 +0530

    Better way to call all args except last atgs

--- src/lib/ee_lib_git.sh
+++ src/lib/ee_lib_git.sh
@@ -2,26 +2,23 @@
 
 function ee_lib_git()
 {
-	for ee_git_dir in $@; do
-		if [ $ee_git_dir != ${@: -1} ]; then
+	for ee_git_dir in ${@:1:$(($#-1))}; do
+		# Change directory
+		cd $ee_git_dir || ee_lib_error "Unable to change directory $ee_git_dir, exit status = " $?
 
-			# Change directory
-			cd $ee_git_dir || ee_lib_error "Unable to change directory $ee_git_dir, exit status = " $?
+		# Check .git
+		if [ ! -d .git ]; then
+			ee_lib_echo "Initialize Git on ${ee_git_dir}"
+			git init &>> $EE_COMMAND_LOG \
+			|| ee_lib_error "Unable to initialize Git on $ee_git_dir, exit status = " $?
+		fi
 
-			# Check .git
-			if [ ! -d .git ]; then
-				ee_lib_echo "Initialize Git on ${ee_git_dir}..."
-				git init &>> $EE_COMMAND_LOG \
-				|| ee_lib_error "Unable to initialize Git on $ee_git_dir, exit status = " $?
-			fi
-
-			# Check for untracked files
-			if [ $(git status -s | wc -l) -ne 0 ]; then
-				# Add files in Git version control
-				ee_lib_echo "Committing changes inside $EE_GIT_DIR, please wait..."
-				git add --all && git commit -am ${@: -1} &>> $EE_COMMAND_LOG \
-				|| ee_lib_error "Unable to Git commit on $ee_git_dir, exit status = " $?
-			fi
+		# Check for untracked files
+		if [ $(git status -s | wc -l) -ne 0 ]; then
+			# Add files in Git version control
+			ee_lib_echo "Git commit on $ee_git_dir, please wait..."
+			git add --all && git commit -am "${@: -1}" &>> $EE_COMMAND_LOG \
+			|| ee_lib_error "Unable to Git commit on $ee_git_dir, exit status = " $?
 		fi
 	done
 }
