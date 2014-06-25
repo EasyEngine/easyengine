66366c7e983f9185729591eebc1d95030fd6b225
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-06-25 14:38:25 +0530

    Fix typo

--- src/modules/site/create/ee_mod_setup_network.sh
+++ src/modules/site/create/ee_mod_setup_network.sh
@@ -2,9 +2,9 @@
 
 function ee_mod_setup_network()
 {
-	cd /var/www/$EE_DOMAIN/htdocs || ee_lib_error "Unable To Change Directory"
+	cd /var/www/$EE_DOMAIN/htdocs || ee_lib_error "Unable to change directory, exit status = " $?
 	wp core install-network --allow-root --title="$EE_WWW_DOMAIN" $EE_WP_SUBDOMAIN &>> $EE_COMMAND_LOG \
-	|| ee_lib_error "Unable to setup WordPress Network, exit status = " $?
+	|| ee_lib_error "Unable to setup WordPress network, exit status = " $?
 
 	sed -i "/WP_DEBUG/a \define('WP_ALLOW_MULTISITE', true);" /var/www/$EE_DOMAIN/wp-config.php
 	sed -i "/WP_ALLOW_MULTISITE/a \define('WPMU_ACCEL_REDIRECT', true);" /var/www/$EE_DOMAIN/wp-config.php
