eda3a7c3bee52e2bb81014380d645c03535c2cdb
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-07-12 18:32:47 +0530

    Dotdeb nginx repo doesn't support spdy

--- src/modules/secure/ee_mod_secure_port.sh
+++ src/modules/secure/ee_mod_secure_port.sh
@@ -8,7 +8,13 @@ function ee_mod_secure_port()
 	if [[ $ee_port = "" ]]; then
 		ee_port=22222
 	fi
-
-	sed -i "s/listen.*/listen $ee_port default_server ssl spdy;/" /etc/nginx/sites-available/22222 \
-	|| ee_lib_error "Unable to change EasyEngine admin port, exit status = " $?
+	
+	if [ "$EE_LINUX_DISTRO" == "Ubuntu" ];	then
+		sed -i "s/listen.*/listen $ee_port default_server ssl spdy;/" /etc/nginx/sites-available/22222 \
+		|| ee_lib_error "Unable to change EasyEngine admin port, exit status = " $?
+	elif [ "$EE_LINUX_DISTRO" == "Debian" ]; then
+		# Dotdeb nginx repository doesn't support spdy
+		sed -i "s/listen.*/listen $ee_port default_server ssl;/" /etc/nginx/sites-available/22222 \
+		|| ee_lib_error "Unable to change EasyEngine admin port, exit status = " $?
+	fi
 }
