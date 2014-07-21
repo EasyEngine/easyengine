fa778689a7bb6f01aece435d32d3ca1427e60784
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-07-21 13:52:07 +0530

    EasyEngine 2.0.1

--- src/lib/ee_lib_variables.sh
+++ src/lib/ee_lib_variables.sh
@@ -1,7 +1,7 @@
 # Define global variables
 
 # EasyEngine version
-readonly EE_VERSION='2.0.0'
+readonly EE_VERSION='2.0.1'
 
 # WP-CLI version
 readonly EE_WP_CLI_VERSION='0.16.0'
--- src/vendor/ee_ven_install_wpcli.sh
+++ src/vendor/ee_ven_install_wpcli.sh
@@ -2,13 +2,14 @@
 
 function ee_ven_install_wpcli()
 {
-	if [ ! -f /usr/local/bin/wp ]; then
+	if [ ! -f /usr/bin/wp ]; then
 		ee_lib_echo "Downloading WP-CLI, please wait..."
-		wget -qO /usr/local/bin/wp https://github.com/wp-cli/wp-cli/releases/download/v${EE_WP_CLI_VERSION}/wp-cli.phar \
+		wget -qO /usr/bin/wp https://github.com/wp-cli/wp-cli/releases/download/v${EE_WP_CLI_VERSION}/wp-cli.phar \
 		|| ee_lib_error "Unable to download WP-CLI, exit status = " $?
 
 		# Executable permission
-		chmod a+x /usr/local/bin/wp
+		chmod a+x /usr/bin/wp \
+		|| ee_lib_error "Unable to set executable permission for wp-cli, exit status = " $?
 
 		# Download auto completion
 		wget -qO /etc/bash_completion.d/wp-completion.bash https://raw.githubusercontent.com/wp-cli/wp-cli/v0.16.0/utils/wp-completion.bash
