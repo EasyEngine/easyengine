b26e621cc47a5d0f87bab20ca47fa3ba932c43e7
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-08 17:50:31 +0530

    Fixed Sieve Error

--- src/modules/stack/install/mail/ee_mod_install_sieve.sh
+++ src/modules/stack/install/mail/ee_mod_install_sieve.sh
@@ -1,9 +1,9 @@
 # Install Sieve package
 
-function ee_mod_install_dovecot()
+function ee_mod_install_sieve()
 {
 	# Install Sieve
 	ee_lib_echo "Installing Sieve, please wait..."
-	$EE_APT_GET amavisd-new spamassassin clamav clamav-daemon arj zoo nomarch cpio lzop cabextract apt-listchanges libauthen-sasl-perl  libdbi-perl libmail-dkim-perl p7zip rpm unrar-free libsnmp-perl \
+	$EE_APT_GET install amavisd-new spamassassin clamav clamav-daemon arj zoo nomarch cpio lzop cabextract apt-listchanges libauthen-sasl-perl  libdbi-perl libmail-dkim-perl p7zip rpm unrar-free libsnmp-perl \
 	|| ee_lib_error "Unable to install Sieve, exit status = " $?
 }
