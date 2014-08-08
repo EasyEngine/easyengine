39d7889d10355939e08f7332680ca52b9201510b
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-08 16:19:01 +0530

    Added Sieve Installation

---
+++ src/modules/stack/install/mail/ee_mod_install_sieve.sh
@@ -0,0 +1,9 @@
+# Install Sieve package
+
+function ee_mod_install_dovecot()
+{
+	# Install Sieve
+	ee_lib_echo "Installing Sieve, please wait..."
+	$EE_APT_GET amavisd-new spamassassin clamav clamav-daemon arj zoo nomarch cpio lzop cabextract apt-listchanges libauthen-sasl-perl  libdbi-perl libmail-dkim-perl p7zip rpm unrar-free libsnmp-perl \
+	|| ee_lib_error "Unable to install Sieve, exit status = " $?
+}
