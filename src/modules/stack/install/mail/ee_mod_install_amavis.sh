2e0dac463e30c259266a8fcbd5de0b2b03eae556
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-21 15:31:08 +0530

    Fixed Amavis installation

--- src/modules/stack/install/mail/ee_mod_install_amavis.sh
+++ src/modules/stack/install/mail/ee_mod_install_amavis.sh
@@ -3,7 +3,7 @@
 function ee_mod_install_amavis()
 {
 	# Install Amavis
-	ee_lib_echo "Installing Amavis, please wait..."
-	$EE_APT_GET amavisd-new spamassassin clamav clamav-daemon arj zoo nomarch cpio lzop cabextract apt-listchanges libauthen-sasl-perl  libdbi-perl libmail-dkim-perl p7zip rpm unrar-free libsnmp-perl \
-	|| ee_lib_error "Unable to install Amavis, exit status = " $?
+	ee_lib_echo "Installing Amavis and ClamAV, please wait..."
+	$EE_APT_GET install amavisd-new spamassassin clamav clamav-daemon arj zoo nomarch cpio lzop cabextract apt-listchanges libauthen-sasl-perl  libdbi-perl libmail-dkim-perl p7zip rpm unrar-free libsnmp-perl \
+	|| ee_lib_error "Unable to install Amavis and ClamAV, exit status = " $?
 }
