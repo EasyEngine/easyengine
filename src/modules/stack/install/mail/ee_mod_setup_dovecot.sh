982e783ee0abb6d9813727df67fdcbef000275c6
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-22 16:16:45 +0530

    Added Certificate for Dovecot and Postfix

--- src/modules/stack/install/mail/ee_mod_setup_dovecot.sh
+++ src/modules/stack/install/mail/ee_mod_setup_dovecot.sh
@@ -2,6 +2,14 @@
 
 function ee_mod_setup_dovecot()
 {
+
+	EE_EMAIL=$($EE_CONFIG_GET wordpress.email)
+	if [[ $EE_EMAIL = "" ]]; then
+		EE_EMAIL=$(git config user.email)
+	fi
+
+	EE_HOSTNAME=$(hostname -f)
+
 	ee_lib_echo "Setting up Dovecot, please wait..."
 	# Adding mail user with GID 5000 and UID 5000
 	groupadd -g 5000 vmail &&	useradd -g vmail -u 5000 vmail -d /var/vmail -m \
@@ -39,4 +47,14 @@ function ee_mod_setup_dovecot()
 	# Change Dovecot log location
 	sed -i "s/#log_path = syslog/log_path = \/var\/log\/dovecot.log/" /etc/dovecot/conf.d/10-logging.conf \
 	|| ee_lib_error "Unable to setup Dovecot log_path, exit status = " $?
+
+	# Configure self signed SSL for Dovecot
+	ee_lib_echo "Generating self signed certificate for Dovecot, please wait..."
+	openssl req -new -x509 -days 3650 -nodes -subj /commonName=${EE_HOSTNAME}/emailAddress=${EE_EMAIL} -out /etc/ssl/certs/dovecot.pem -keyout /etc/ssl/private/dovecot.pem &>> $EE_COMMAND_LOG
+
+	# Setting up certificate in file
+	sed -i "s'/etc/dovecot/dovecot.pem'/etc/ssl/certs/dovecot.pem'" /etc/dovecot/conf.d/10-ssl.conf \
+	sed -i "s'/etc/dovecot/private/dovecot.pem'/etc/ssl/private/dovecot.pem'" /etc/dovecot/conf.d/10-ssl.conf \
+	|| ee_lib_error "Unable to setup Dovecot SSL certificate path, exit status = " $?
+
 }
