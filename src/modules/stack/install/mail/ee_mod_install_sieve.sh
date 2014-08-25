22b1b368ab6fad7b3387b5a2d6f2fa2cda986fa7
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-25 17:35:16 +0530

    Fixed Wrong package names

--- src/modules/stack/install/mail/ee_mod_install_sieve.sh
+++ src/modules/stack/install/mail/ee_mod_install_sieve.sh
@@ -4,6 +4,6 @@ function ee_mod_install_sieve()
 {
 	# Install Sieve
 	ee_lib_echo "Installing Sieve, please wait..."
-	$EE_APT_GET install apt-get install dovecot-sieve dovecot-managesieved \
+	$EE_APT_GET install dovecot-sieve dovecot-managesieved \
 	|| ee_lib_error "Unable to install Sieve, exit status = " $?
 }
