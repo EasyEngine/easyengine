1bebdc2f5f918af6bc807496cb203fc1ea9b2b08
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-27 13:40:31 +0530

    Removed Missing packages

--- src/modules/stack/install/mail/ee_mod_install_mailscan.sh
+++ src/modules/stack/install/mail/ee_mod_install_mailscan.sh
@@ -4,6 +4,6 @@ function ee_mod_install_mailscan()
 {
 	# Install Amavis
 	ee_lib_echo "Installing Amavis, SpamAssassin and ClamAV, please wait..."
-	$EE_APT_GET install amavisd-new spamassassin clamav clamav-daemon arj zoo nomarch cpio lzop cabextract apt-listchanges libauthen-sasl-perl  libdbi-perl libmail-dkim-perl p7zip rpm unrar-free libsnmp-perl \
+	$EE_APT_GET install amavisd-new spamassassin clamav clamav-daemon arj zoo nomarch cpio lzop cabextract p7zip rpm unrar-free \
 	|| ee_lib_error "Unable to install Amavis, SpamAssassin and ClamAV, exit status = " $?
 }
