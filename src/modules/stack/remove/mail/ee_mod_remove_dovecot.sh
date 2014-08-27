efe9cdd0f9fbf87d14c4b03fe930200e81646663
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-27 13:22:08 +0530

    Added mail remove and purge commands

---
+++ src/modules/stack/remove/mail/ee_mod_remove_dovecot.sh
@@ -0,0 +1,9 @@
+# Remove Dovecot package
+
+function ee_mod_remove_dovecot()
+{
+	ee_lib_echo "$EE_SECOND Dovecot package, please wait..."
+	$EE_APT_GET $EE_SECOND dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql \
+	|| ee_lib_error "Unable to $EE_SECOND Dovecot, exit status = " $?
+
+}
