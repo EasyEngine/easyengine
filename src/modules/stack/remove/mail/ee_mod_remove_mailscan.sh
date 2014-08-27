e52ceb3b5f95ec05212cb35e90fab1de3b546f9f
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-27 14:53:07 +0530

    Removed unwanted packages

--- src/modules/stack/remove/mail/ee_mod_remove_mailscan.sh
+++ src/modules/stack/remove/mail/ee_mod_remove_mailscan.sh
@@ -3,7 +3,7 @@
 function ee_mod_remove_mailscan()
 {
 	ee_lib_echo "$EE_SECOND Amavis, SpamAssassin and ClamAV package, please wait..."
-	$EE_APT_GET $EE_SECOND amavisd-new spamassassin clamav clamav-daemon arj zoo nomarch cpio lzop cabextract p7zip rpm unrar-free \
+	$EE_APT_GET $EE_SECOND amavisd-new spamassassin clamav clamav-daemon arj zoo nomarch lzop cabextract p7zip rpm unrar-free \
 	|| ee_lib_error "Unable to $EE_SECOND Amavis, SpamAssassin and ClamAV,, exit status = " $?
 
 }
