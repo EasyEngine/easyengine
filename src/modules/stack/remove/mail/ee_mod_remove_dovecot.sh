3c6d3497eec5ad0e39a07705968e5609cb43bf59
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-29 18:30:05 +0530

    Otimized Code

--- src/modules/stack/remove/mail/ee_mod_remove_dovecot.sh
+++ src/modules/stack/remove/mail/ee_mod_remove_dovecot.sh
@@ -3,7 +3,7 @@
 function ee_mod_remove_dovecot()
 {
 	ee_lib_echo "$EE_SECOND Dovecot package, please wait..."
-	$EE_APT_GET $EE_SECOND dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql \
+	$EE_APT_GET $EE_SECOND dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql dovecot-sieve dovecot-managesieved \
 	|| ee_lib_error "Unable to $EE_SECOND Dovecot, exit status = " $?
 
 	userdel -rf vmail || ee_lib_error "Unable to Remove user vmail, exit status = " $?
