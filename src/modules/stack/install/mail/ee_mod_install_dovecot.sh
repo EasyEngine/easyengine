4ba2c91d58c39b59bc4d755853585efdf8466397
gau1991 <gaurav.astikar@rtcamp.com>
2014-08-29 18:30:05 +0530

    Otimized Code

--- src/modules/stack/install/mail/ee_mod_install_dovecot.sh
+++ src/modules/stack/install/mail/ee_mod_install_dovecot.sh
@@ -6,7 +6,7 @@ function ee_mod_install_dovecot()
 	ee_lib_echo "Installing Dovecot, please wait..."
 	debconf-set-selections <<< "dovecot-core dovecot-core/create-ssl-cert boolean yes"
 	debconf-set-selections <<< "dovecot-core dovecot-core/ssl-cert-name string $(hostname -f)"
-	$EE_APT_GET install dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql \
+	$EE_APT_GET install dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql dovecot-sieve dovecot-managesieved \
 	|| ee_lib_error "Unable to install Dovecot, exit status = " $?
 
 }
--- src/modules/stack/install/mail/ee_mod_install_sieve.sh
+++
@@ -1,9 +0,0 @@
-# Install Sieve package
-
-function ee_mod_install_sieve()
-{
-	# Install Sieve
-	ee_lib_echo "Installing Sieve, please wait..."
-	$EE_APT_GET install dovecot-sieve dovecot-managesieved \
-	|| ee_lib_error "Unable to install Sieve, exit status = " $?
-}
