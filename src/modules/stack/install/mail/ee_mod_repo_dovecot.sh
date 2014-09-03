05a47ff27b8019fea469c89f1a6b89b3a2bff7f6
gau1991 <gaurav.astikar@rtcamp.com>
2014-09-03 15:31:49 +0530

    Changed Dovcot Repo

--- src/modules/stack/install/mail/ee_mod_repo_dovecot.sh
+++ src/modules/stack/install/mail/ee_mod_repo_dovecot.sh
@@ -5,15 +5,9 @@ function ee_mod_repo_dovecot()
 	if [ "$EE_DEBIAN_VERSION" == "squeeze" ];then
 
 		# Add Dovecot repository
-		# Ref:http://wiki2.dovecot.org/PrebuiltBinaries
-
 		ee_lib_echo "Adding Dovecot repository, please wait..."
-		echo "deb http://xi.rename-it.nl/debian/ oldstable-auto/dovecot-2.2 main" > /etc/apt/sources.list.d/dovecot-$(lsb_release -sc).list \
+		echo "deb http://http.debian.net/debian-backports squeeze-backports main" > /etc/apt/sources.list.d/dovecot-$(lsb_release -sc).list \
 		|| ee_lib_error "Unable to add Dovecot repository, exit status = " $?
-
-		# Fetch and install Dovecot GnuPG key
-		wget -O - http://xi.rename-it.nl/debian/archive.key | apt-key add - \
-		|| ee_lib_error "Unable to fetch Dovecot GnuPG key, exit status = " $?
 
 	fi
 }
