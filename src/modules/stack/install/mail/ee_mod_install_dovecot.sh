7553dfb592cbae4bdef930d9977dd4ddd4c16262
gau1991 <gaurav.astikar@rtcamp.com>
2014-09-03 14:57:31 +0530

    Added Dovecot repo for Debian 6

--- src/modules/stack/install/mail/ee_mod_install_dovecot.sh
+++ src/modules/stack/install/mail/ee_mod_install_dovecot.sh
@@ -2,6 +2,10 @@
 
 function ee_mod_install_dovecot()
 {
+	# Add Dovecot repo for Debian 6
+	ee_mod_repo_dovecot
+	ee_lib_apt_get_update
+
 	# Install Dovecot
 	ee_lib_echo "Installing Dovecot, please wait..."
 	debconf-set-selections <<< "dovecot-core dovecot-core/create-ssl-cert boolean yes"
