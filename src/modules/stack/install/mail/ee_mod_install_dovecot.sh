cdb6dbd63643dc076a8cca26eeed370028340650
gau1991 <gaurav.astikar@rtcamp.com>
2014-09-03 15:31:49 +0530

    Changed Dovcot Repo

--- src/modules/stack/install/mail/ee_mod_install_dovecot.sh
+++ src/modules/stack/install/mail/ee_mod_install_dovecot.sh
@@ -10,7 +10,14 @@ function ee_mod_install_dovecot()
 	ee_lib_echo "Installing Dovecot, please wait..."
 	debconf-set-selections <<< "dovecot-core dovecot-core/create-ssl-cert boolean yes"
 	debconf-set-selections <<< "dovecot-core dovecot-core/ssl-cert-name string $(hostname -f)"
-	$EE_APT_GET install dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql dovecot-sieve dovecot-managesieved \
-	|| ee_lib_error "Unable to install Dovecot, exit status = " $?
+
+	# Debian 6 doesn't provide Dovecot 2.x
+	if [ "$EE_DEBIAN_VERSION" == "squeeze" ]; then
+		$EE_APT_GET  -t squeeze-backports install dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql dovecot-sieve dovecot-managesieved \
+		|| ee_lib_error "Unable to install Dovecot, exit status = " $?
+	else
+		$EE_APT_GET install dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql dovecot-sieve dovecot-managesieved \
+		|| ee_lib_error "Unable to install Dovecot, exit status = " $?
+	fi
 
 }
