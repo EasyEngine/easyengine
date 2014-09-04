939699aee87ac2f6c6f10a635f72d60d065dd18e
gau1991 <gaurav.astikar@rtcamp.com>
2014-09-04 19:00:48 +0530

    Fixed Error in Vim Editor

--- src/modules/site/ee_mod_site_edit.sh
+++ src/modules/site/ee_mod_site_edit.sh
@@ -2,5 +2,10 @@
 
 function ee_mod_site_edit()
 {
-	sensible-editor /etc/nginx/sites-available/$EE_DOMAIN $1
+	grep vim ~/.selected_editor &>> $EE_COMMAND_LOG
+	if [ $? -eq 0 ]; then
+		sensible-editor /etc/nginx/sites-available/$EE_DOMAIN $1 2> /dev/null
+	else
+		sensible-editor /etc/nginx/sites-available/$EE_DOMAIN $1
+	fi
 }
