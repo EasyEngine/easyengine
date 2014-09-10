ebd151d2dfee2d92b308d68e8761073841f15e14
harshadyeola <harshad.yeola@rtcamp.com>
2014-09-10 11:01:30 +0530

    function ee_site_cd

---
+++ src/modules/site/ee_site_cd.sh
@@ -0,0 +1,7 @@
+#CD to Webroot
+
+function ee_site_cd()
+{
+	cd $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g') \
+	|| ee_lib_error "Unable to change directory for $EE_DOMAIN, exit status = " $?
+}
