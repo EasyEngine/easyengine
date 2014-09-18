6f3e7d067ab8ff8c93bdb37b0bdf7ea226fc3e12
gau1991 <gaurav.astikar@rtcamp.com>
2014-09-18 16:23:17 +0530

    Added ee import-slow-log command

--- src/lib/ee_lib_import_slow_log.sh
+++ src/lib/ee_lib_import_slow_log.sh
@@ -3,15 +3,20 @@
 function ee_lib_import_slow_log()
 {
 
-	dpkg --compare-versions $(pt-query-digest --version | awk '{print $2 }') ge 2.2
-	if [ $? -eq 0 ]; then
-		ee_anemometer_history=history
-	else
-		ee_anemometer_history=review-history
-	fi
+	if [ -d /var/www/22222/htdocs/db/anemometer ]; then
+		ee_lib_echo "Importing MySQL slow log, please wait..."
+		dpkg --compare-versions $(pt-query-digest --version | awk '{print $2 }') ge 2.2
+		if [ $? -eq 0 ]; then
+			ee_anemometer_history=history
+		else
+			ee_anemometer_history=review-history
+		fi
 
-	pt-query-digest --user=anemometer --password=anemometer_password \
-	--review D=slow_query_log,t=global_query_review \
-	--${ee_anemometer_history} D=slow_query_log,t=global_query_review_history \
-	--no-report --limit=0% --filter=" \$event->{Bytes} = length(\$event->{arg}) and \$event->{hostname}=\"$EE_MYSQL_GRANT_HOST\"" /var/log/mysql/mysql-slow.log
+		pt-query-digest --user=anemometer --password=anemometer_password \
+		--review D=slow_query_log,t=global_query_review \
+		--${ee_anemometer_history} D=slow_query_log,t=global_query_review_history \
+		--no-report --limit=0% --filter=" \$event->{Bytes} = length(\$event->{arg}) and \$event->{hostname}=\"$EE_MYSQL_GRANT_HOST\"" /var/log/mysql/mysql-slow.log
+	else
+		ee_lib_echo_fail "Anememoter is not installed"
+	fi
 }
