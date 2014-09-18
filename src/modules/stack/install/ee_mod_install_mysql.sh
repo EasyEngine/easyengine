68b91cc3b4408db6b2b17ec72c4dcd0ee27f116a
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-09-18 12:55:21 +0530

    Fix message on mysql unable to install

--- src/modules/stack/install/ee_mod_install_mysql.sh
+++ src/modules/stack/install/ee_mod_install_mysql.sh
@@ -23,7 +23,7 @@ ee_mod_install_mysql()
 
 	ee_lib_echo "Installing Percona MySQL, please Wait..."
 	$EE_APT_GET install percona-server-server-5.6 mysqltuner percona-toolkit \
-	|| ee_lib_error "Unable to install MySQL, exit status = " $?
+	|| ee_lib_error "Unable to install Percona MySQL, exit status = " $?
 
 	# Download tuning-primer.sh
 	wget --no-check-certificate -cqO /usr/local/bin/tuning-primer.sh https://launchpadlibrarian.net/78745738/tuning-primer.sh
