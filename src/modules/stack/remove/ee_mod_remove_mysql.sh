37803b2950edd1d4407575d74b983a3f3b421424
harshadyeola <harshad.yeola@rtcamp.com>
2014-09-23 12:50:17 +0530

    purge mysql-server or percona-server

--- src/modules/stack/remove/ee_mod_remove_mysql.sh
+++ src/modules/stack/remove/ee_mod_remove_mysql.sh
@@ -2,8 +2,14 @@
 
 function ee_mod_remove_mysql()
 {
+	dpkg --get-selections | grep -v deinstall | grep mysql-server &>> $EE_COMMAND_LOG
+	if [ $? -eq 0 ]; then
+		ee_mysql_server=mysql-server
+	else
+		ee_mysql_server=percona-server-server-5.6
+	fi
 	ee_lib_echo "$EE_SECOND Percona MySQL package, please wait..."
-	$EE_APT_GET $EE_SECOND percona-server-server-5.6 mysqltuner percona-toolkit \
+	$EE_APT_GET $EE_SECOND $ee_mysql_server mysqltuner percona-toolkit \
 	|| ee_lib_error "Unable to $EE_SECOND Percona MySQL, exit status = " $?
 
 	# Remove tuning-primer.sh
