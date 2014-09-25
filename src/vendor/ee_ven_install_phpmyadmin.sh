7d5a095351c0073e6b877dd6f56e8d15cd2bd15a
gau1991 <gaurav.astikar@rtcamp.com>
2014-09-25 15:34:51 +0530

    Fixes  #301

--- src/vendor/ee_ven_install_phpmyadmin.sh
+++ src/vendor/ee_ven_install_phpmyadmin.sh
@@ -4,7 +4,9 @@ function ee_ven_install_phpmyadmin()
 {
 	if [ ! -d /var/www/22222/htdocs/db/pma ]; then
 
-		# Setup phpMyAdmin
+		local ee_random=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n1)
+
+		# Setup phpMyAdmin directory
 		mkdir -p /var/www/22222/htdocs/db/pma/ \
 		|| ee_lib_error "Unable to create phpMyAdmin directory: /var/www/22222/htdocs/db/pma/, exit status = " $?
 
@@ -19,6 +21,13 @@ function ee_ven_install_phpmyadmin()
 
 		# Remove unwanted files
 		rm -f /var/www/22222/htdocs/db/pma/pma.tar.gz
+
+		# Setup phpMyAdmin
+		cp -v /var/www/22222/htdocs/db/pma/config.sample.inc.php /var/www/22222/htdocs/db/pma/config.inc.php &>> $EE_COMMAND LOG \
+		|| ee_lib_error "Unable to setup phpMyAdmin, exit status = " $?
+
+		sed -i "s/a8b7c6d/$ee_random/" /var/www/22222/htdocs/db/pma/config.inc.php \
+		|| ee_lib_error "Unable to setup phpMyAdmin, exit status = " $?
 
 	fi
 }
