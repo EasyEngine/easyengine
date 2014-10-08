a455821299902cca08f6ac3d6a381f4f33bfad39
harshadyeola <harshad.yeola@rtcamp.com>
2014-10-08 19:19:54 +0530

    added message while backup

--- src/modules/site/ee_mod_site_backup.sh
+++ src/modules/site/ee_mod_site_backup.sh
@@ -10,15 +10,18 @@ function ee_mod_site_backup()
 
 		# Move htdocs
 		if [ "$EE_SITE_CURRENT_OPTION" = "HTML" ] || [ "$EE_SITE_CURRENT_OPTION" = "MYSQL" ] || [ "$EE_SITE_CURRENT_OPTION" = "PHP" ]; then
+			ee_lib_echo "Creating Webroot backup for $EE_DOMAIN before updating ..."
 			mv $ee_webroot/htdocs $ee_webroot/backup/htdocs/htdocs-$(date +"%m-%d-%y::%T") || ee_lib_error "Unable to create $ee_webroot/htdocs backup, exit status =" $?
 			mkdir -p $ee_webroot/htdocs
 		fi
 
+		ee_lib_echo "Creating NGINX configuration backup for $EE_DOMAIN before updating ..."
 		# Backup $EE_DOMAIN NGINX configuration
 		cp -av /etc/nginx/sites-available/$EE_DOMAIN $ee_webroot/backup/nginx/${EE_DOMAIN}-$(date +"%m-%d-%y::%T").conf.bak &>> $EE_COMMAND_LOG
 
 		# Database backup
 		if [ -f $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/*-config.php/') ]; then
+			ee_lib_echo "Creating Database backup for $EE_DOMAIN before updating ..."
 			local ee_db_name=$(grep DB_NAME $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/*-config.php/' 2> /dev/null) | cut -d"'" -f4)
 			mysqldump $ee_db_name > $ee_webroot/backup/db/${ee_db_name}-$(date +"%m-%d-%y::%T").sql.bak &>> $EE_COMMAND_LOG
 		fi
