c3bff10b284949fbb5b6be2aff5840daa794d114
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-10-08 13:36:08 +0530

    Reuse code and comment codes

---
+++ src/modules/site/ee_mod_site_packages.sh
@@ -0,0 +1,21 @@
+# Install required packages
+
+if [ "$EE_SITE_CREATE_OPTION" = "--html" ] || [ "$EE_SITE_CREATE_OPTION" = "--php" ] || [ "$EE_SITE_CREATE_OPTION" = "--mysql" ] || [ "$EE_SITE_CREATE_OPTION" = "--wp" ] || [ "$EE_SITE_CREATE_OPTION" = "--wpsubdir" ] || [ "$EE_SITE_CREATE_OPTION" = "--wpsubdomain" ]; then
+	# Check & Install NGINX Packages
+	ee_lib_stack_packages nginx
+fi
+if [ "$EE_SITE_CREATE_OPTION" = "--php" ] || [ "$EE_SITE_CREATE_OPTION" = "--mysql" ] || [ "$EE_SITE_CREATE_OPTION" = "--wp" ] || [ "$EE_SITE_CREATE_OPTION" = "--wpsubdir" ] || [ "$EE_SITE_CREATE_OPTION" = "--wpsubdomain" ]; then
+	# Check & Install PHP Packages
+	ee_lib_stack_packages php
+fi
+if [ "$EE_SITE_CREATE_OPTION" = "--mysql" ] || [ "$EE_SITE_CREATE_OPTION" = "--wp" ] || [ "$EE_SITE_CREATE_OPTION" = "--wpsubdir" ] || [ "$EE_SITE_CREATE_OPTION" = "--wpsubdomain" ]; then
+	# Check & Install Percona MySQL Packages
+	ee_lib_stack_packages mysql
+fi
+
+if [ "$EE_SITE_CREATE_OPTION" = "--wp" ] || [ "$EE_SITE_CREATE_OPTION" = "--wpsubdir" ] || [ "$EE_SITE_CREATE_OPTION" = "--wpsubdomain" ]; then
+	# Install WP-CLI
+	ee_ven_install_wpcli
+fi
+# Check & Install Postfix Packages
+ee_lib_stack_packages postfix
