84f967674d54af44e7c6d1157d7de07e7e1812c2
harshadyeola <harshad.yeola@rtcamp.com>
2014-10-08 18:11:41 +0530

    updated messages and cache change code

--- src/modules/site/update/ee_mod_update_plugins.sh
+++ src/modules/site/update/ee_mod_update_plugins.sh
@@ -6,14 +6,15 @@ function ee_mod_update_plugins()
 
 	if [ "$EE_SITE_CURRENT_OPTION" = "WPSINGLE W3 TOTAL CACHE" ] || [ "$EE_SITE_CURRENT_OPTION" = "WPSINGLE FAST CGI" ] \
 		|| [ "$EE_SITE_CURRENT_OPTION" = "WPSUBDIR W3 TOTAL CACHE" ] || [ "$EE_SITE_CURRENT_OPTION" = "WPSUBDIR FAST CGI" ] \
-		|| [ "$EE_SITE_CURRENT_OPTION" = "WPSUBDOMAIN W3 TOTAL CACHE" ] || [ "$EE_SITE_CURRENT_OPTION" = "WPSUBDOMAIN FAST CGI" ] && [ "$EE_SITE_CACHE_OPTION" = "--basic" ]; then
+		|| [ "$EE_SITE_CURRENT_OPTION" = "WPSUBDOMAIN W3 TOTAL CACHE" ] || [ "$EE_SITE_CURRENT_OPTION" = "WPSUBDOMAIN FAST CGI" ] && [[ "$EE_SITE_CACHE_OPTION" = "--basic" || "$EE_SITE_CACHE_OPTION" = "--wpsc" ]]; then
 		ee_lib_echo "Uninstalling W3 Total Cache plugin, please wait..."
 		wp plugin --allow-root uninstall w3-total-cache &>> $EE_COMMAND_LOG 
 	fi
 
 	if [ "$EE_SITE_CURRENT_OPTION" = "WPSUBDOMAIN WP SUPER CACHE" ] || [ "$EE_SITE_CURRENT_OPTION" = "WPSINGLE WP SUPER CACHE" ] \
 		|| [ "$EE_SITE_CURRENT_OPTION" = "WPSUBDIR WP SUPER CACHE" ] && [ "$EE_SITE_CACHE_OPTION" = "--basic" ]; then
-		ee_lib_echo "Unnstalling WP Super Cache plugin, please wait..."
+		ee_lib_echo "Unistalling WP Super Cache plugin, please wait..."
+
 		wp plugin --allow-root uninstall wp-super-cache &>> $EE_COMMAND_LOG
 	fi
 
