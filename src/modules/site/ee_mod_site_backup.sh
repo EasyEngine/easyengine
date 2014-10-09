a465001ce748e76b9ff7c5cc59712c4f5ba8ffa2
harshadyeola <harshad.yeola@rtcamp.com>
2014-10-09 16:42:28 +0530

    error messages updated

--- src/modules/site/ee_mod_site_backup.sh
+++ src/modules/site/ee_mod_site_backup.sh
@@ -11,27 +11,27 @@ function ee_mod_site_backup()
 
 		# Move htdocs
 		if [ "$EE_SITE_CURRENT_OPTION" = "HTML" ] || [ "$EE_SITE_CURRENT_OPTION" = "MYSQL" ] || [ "$EE_SITE_CURRENT_OPTION" = "PHP" ]; then
-			ee_lib_echo "Creating Webroot backup for $EE_DOMAIN before updating ..."
+			ee_lib_echo "Creating Webroot backup for $EE_DOMAIN, please wait..."
 			mv $ee_webroot/htdocs $ee_webroot/backup/htdocs/$EE_DATE/ || ee_lib_error "Unable to move $ee_webroot/htdocs to backup, exit status =" $?
 			mkdir -p $ee_webroot/htdocs
 		fi
 
-		ee_lib_echo "Creating NGINX configuration backup for $EE_DOMAIN before updating ..."
+		ee_lib_echo "Creating NGINX configuration backup for $EE_DOMAIN, please wait..."
 		# Backup $EE_DOMAIN NGINX configuration
-		cp -av /etc/nginx/sites-available/$EE_DOMAIN $ee_webroot/backup/nginx/${EE_DOMAIN}-$EE_DATE.conf.bak &>> $EE_COMMAND_LOG
+		cp /etc/nginx/sites-available/$EE_DOMAIN $ee_webroot/backup/nginx/${EE_DOMAIN}-$EE_DATE.conf &>> $EE_COMMAND_LOG
 
 		# Database backup
 		if [ -f $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/*-config.php/') ]; then
-			ee_lib_echo "Creating Database backup for $EE_DOMAIN before updating ..."
+			ee_lib_echo "Creating Database backup for $EE_DOMAIN, please wait..."
 			local ee_db_name=$(grep DB_NAME $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/*-config.php/' 2> /dev/null) | cut -d"'" -f4)
-			mysqldump $ee_db_name > $ee_webroot/backup/db/${ee_db_name}-$EE_DATE.sql.bak \
+			mysqldump $ee_db_name > $ee_webroot/backup/db/${ee_db_name}-$EE_DATE.sql \
 			|| ee_lib_error "Unable to dump ${ee_db_name}, exit status =" $?
 
 			# Move ee-config.php and copy wp-config.php to backup
 			if [ -f $ee_webroot/ee-config.php ]; then
 				mv $ee_webroot/ee-config.php $ee_webroot/backup/htdocs/$EE_DATE/ || ee_lib_error "Unable to move $ee_webroot/ee-config.php to backup, exit status =" $?
 			else
-				cp $ee_webroot/wp-config.php $ee_webroot/backup/htdocs/$EE_DATE/ || ee_lib_error "Unable to move $ee_webroot/ee-config.php to backup, exit status =" $?
+				cp $ee_webroot/wp-config.php $ee_webroot/backup/htdocs/$EE_DATE/ || ee_lib_error "Unable to move $ee_webroot/wp-config.php to backup, exit status =" $?
 			fi
 		fi
 }
