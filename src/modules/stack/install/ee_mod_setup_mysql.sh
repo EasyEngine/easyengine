61306ce7cabfab9fa44131c9414e32cb2d6dc0ba
gau1991 <gaurav.astikar@rtcamp.com>
2014-10-30 17:29:21 +0530

    Disable MySQL  performance_schema by default #326

--- src/modules/stack/install/ee_mod_setup_mysql.sh
+++ src/modules/stack/install/ee_mod_setup_mysql.sh
@@ -6,11 +6,11 @@ function ee_mod_setup_mysql()
 
 	# Setting wait_timeout = 30 & interactive_timeout = 60
 	if [ ! -f /etc/mysql/my.cnf ]; then
-		echo  -e "[mysqld] \nwait_timeout = 30 \ninteractive_timeout = 60" > /etc/mysql/my.cnf
+		echo  -e "[mysqld] \nwait_timeout = 30 \ninteractive_timeout = 60 \nperformance_schema = 0" > /etc/mysql/my.cnf
 	else
 		grep "_timeout" /etc/mysql/my.cnf &>> $EE_COMMAND_LOG
 		if [ $? -ne 0 ]; then
-			sed -i "/#max_connections/a wait_timeout = 30 \ninteractive_timeout = 60" /etc/mysql/my.cnf
+			sed -i "/#max_connections/a wait_timeout = 30 \ninteractive_timeout = 60 \nperformance_schema = 0" /etc/mysql/my.cnf
 		fi
 	fi
 }
