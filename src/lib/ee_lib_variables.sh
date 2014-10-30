8a745e33ded04bde8f43cbec974e6bffe66ad219
Mitesh Shah <Mr.Miteshah@gmail.com>
2014-10-30 18:20:00 +0530

    Minor update

--- src/lib/ee_lib_variables.sh
+++ src/lib/ee_lib_variables.sh
@@ -20,7 +20,8 @@ readonly EE_DATE=$(date +%d%b%Y%H%M%S)
 
 # Log only single time
 # ee site create example.com called ee stack install nginx
-# So in log file all logs written twice
+# So when ee stack install nginx run in sub-shell the value of EE_COMMAND_LOG=/dev/null
+# So in log file all logs written single time only
 if [ -n "$EE_LOG" ]; then
         EE_COMMAND_LOG=/dev/null
 else
