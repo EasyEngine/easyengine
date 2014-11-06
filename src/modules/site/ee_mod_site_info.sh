4cdf055ec442d28cdbb73b5d79034987c5590579
gau1991 <gaurav.astikar@rtcamp.com>
2014-11-06 16:48:48 +0530

    Display DB_HOST in ee site info

--- src/modules/site/ee_mod_site_info.sh
+++ src/modules/site/ee_mod_site_info.sh
@@ -7,7 +7,7 @@ function ee_mod_site_info()
 	local ee_access_log=$(grep access_log /etc/nginx/sites-available/$EE_DOMAIN | grep "/var/log/nginx/" | awk '{print($2)}' | cut -d ';' -f1)
 	local ee_error_log=$(grep error_log /etc/nginx/sites-available/$EE_DOMAIN | grep "/var/log/nginx/" | awk '{print($2)}' | cut -d ';' -f1)
 	local ee_webroot=$(grep root /etc/nginx/sites-available/$EE_DOMAIN | grep htdocs | awk '{print($2)}'  | cut -d ';' -f1)
-	
+
 	ee_lib_echo "Information about $EE_DOMAIN:"
 	ee_lib_echo_escape "\nNginx configuration\t \033[37m$ee_site_info ($ee_site_status)"
 	ee_lib_echo_escape "access_log\t\t \033[37m$ee_access_log"
@@ -16,17 +16,19 @@ function ee_mod_site_info()
 
 	if [ -f $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/*-config.php/') ]; then
 		local ee_db_name=$(grep DB_NAME $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/*-config.php/' 2> /dev/null) | cut -d"'" -f4)
+		local ee_db_host=$(grep DB_HOST $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/*-config.php/' 2> /dev/null) | cut -d"'" -f4)
 		local ee_db_user=$(grep DB_USER $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/*-config.php/' 2> /dev/null) | cut -d"'" -f4)
 		local ee_db_pass=$(grep DB_PASS $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/*-config.php/' 2> /dev/null) | cut -d"'" -f4)
 
+		ee_lib_echo_escape "DB_HOST\t\t\t \033[37m$ee_db_host"
 		ee_lib_echo_escape "DB_NAME\t\t\t \033[37m$ee_db_name"
 		ee_lib_echo_escape "DB_USER\t\t\t \033[37m$ee_db_user"
 		ee_lib_echo_escape "DB_PASS\t\t\t \033[37m$ee_db_pass"
-		
+
 		if [ -f $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/wp-config.php/') ]; then
 			local ee_table_prefix=$(grep table_prefix $(grep root /etc/nginx/sites-available/$EE_DOMAIN | awk '{ print $2 }' | sed 's/;//g' | sed 's/htdocs/wp-config.php/') | cut -d"'" -f2)
 			ee_lib_echo_escape "table_prefix\t\t \033[37m$ee_table_prefix"
-		fi 
+		fi
 	fi
 
 }
