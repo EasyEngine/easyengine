b9702673ac57f4c8751304b691803fd53376b058
gau1991 <gaurav.astikar@rtcamp.com>
2014-12-04 14:01:56 +0530

    Added apt-get core function structure

---
+++ ee/core/aptget.py
@@ -0,0 +1,22 @@
+"""EasyEngine package installation using apt-get module."""
+
+Class EEAptGet():
+    """Generice apt-get intialisation"""
+    def __init__():
+        # TODO Common method of apt-get module
+        pass
+
+    """Installation of packages"""
+    def install():
+        # TODO Method to install packages
+        pass
+
+    """Removal of packages"""
+    def remove():
+        # TODO Method to remove packages
+        pass
+
+    """Purging of packages"""
+    def purge():
+        # TODO Method to purge packages
+        pass
