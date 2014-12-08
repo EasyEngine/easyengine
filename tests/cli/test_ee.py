6a1f7bd2cb742051e5c3ee113c834d18c5d2b3ed
harshadyeola <harshad.yeola@rtcamp.com>
2014-12-08 13:12:16 +0530

    Output Handler Mustache

--- tests/cli/test_ee.py
+++ tests/cli/test_ee.py
@@ -11,55 +11,57 @@ class CliTestCase(test.EETestCase):
         self.app.close()
 
     def test_ee_cli_site_create(self):
-        app = get_test_app(argv=['site', 'create', 'example.com'])
+        self.app = get_test_app(argv=['site', 'create', 'example.com'])
         self.app.setup()
         self.app.run()
+        data, output = self.app.last_rendered
+        self.eq(output, 'Inside EESiteCreateController.default().\n')
         self.app.close()
 
     def test_ee_cli_site_update(self):
-        app = get_test_app(argv=['site', 'update', 'example.com'])
+        self.app = get_test_app(argv=['site', 'update', 'example.com'])
         self.app.setup()
         self.app.run()
         self.app.close()
 
     def test_ee_cli_site_delete(self):
-        app = get_test_app(argv=['site', 'delete', 'example.com'])
+        self.app = get_test_app(argv=['site', 'delete', 'example.com'])
         self.app.setup()
         self.app.run()
         self.app.close()
 
     def test_ee_cli_site_cd(self):
-        app = get_test_app(argv=['site', 'cd', 'example.com'])
+        self.app = get_test_app(argv=['site', 'cd', 'example.com'])
         self.app.setup()
         self.app.run()
         self.app.close()
 
     def test_ee_cli_site_log(self):
-        app = get_test_app(argv=['site', 'log', 'example.com'])
+        self.app = get_test_app(argv=['site', 'log', 'example.com'])
         self.app.setup()
         self.app.run()
         self.app.close()
 
     def test_ee_cli_site_info(self):
-        app = get_test_app(argv=['site', 'info', 'example.com'])
+        self.app = get_test_app(argv=['site', 'info', 'example.com'])
         self.app.setup()
         self.app.run()
         self.app.close()
 
     def test_ee_cli_site_enable(self):
-        app = get_test_app(argv=['site', 'enable', 'example.com'])
+        self.app = get_test_app(argv=['site', 'enable', 'example.com'])
         self.app.setup()
         self.app.run()
         self.app.close()
 
     def test_ee_cli_site_disable(self):
-        app = get_test_app(argv=['site', 'disable', 'example.com'])
+        self.app = get_test_app(argv=['site', 'disable', 'example.com'])
         self.app.setup()
         self.app.run()
         self.app.close()
 
     def test_ee_cli_site_edit(self):
-        app = get_test_app(argv=['site', 'edit', 'example.com'])
+        self.app = get_test_app(argv=['site', 'edit', 'example.com'])
         self.app.setup()
         self.app.run()
         self.app.close()
