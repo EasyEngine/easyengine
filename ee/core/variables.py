7670c2eeba6a1840c644e5f903f84ca70896438c
gau1991 <gaurav.astikar@rtcamp.com>
2014-12-10 13:05:00 +0530

    Addded some EasyEngine variables to work with satck

--- ee/core/variables.py
+++ ee/core/variables.py
@@ -4,5 +4,14 @@
 class EEVariables():
     """Intialization of core variables"""
     def ___init__():
-        # TODO method for core variables
-        pass
+
+        #EasyEngine core variables
+        ee_version
+
+        #EasyEngine stack installation varibales
+        ee_nginx_repo = "ppa:rtcamp/nginx"
+        ee_nginx = "nginx-full"
+        ee_php_repo = "ppa:ondrej/php5"
+        ee_php = "php5"
+        ee_mysql_repo = ""
+        ee_mysql = ""
