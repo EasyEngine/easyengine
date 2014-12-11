e4e7dfd93233923bd2faf1ecb5a6f4cf9d2da7ac
gau1991 <gaurav.astikar@rtcamp.com>
2014-12-11 11:47:04 +0530

    Changed stack to work as plugin

--- ee/cli/bootstrap.py
+++ ee/cli/bootstrap.py
@@ -8,19 +8,18 @@ from ee.cli.controllers.base import EEBaseController
 from ee.cli.controllers.site import EESiteController
 from ee.cli.controllers.site import EESiteCreateController
 from ee.cli.controllers.site import EESiteUpdateController
-from ee.cli.controllers.stack import EEStackController
 from ee.cli.controllers.debug import EEDebugController
 from ee.cli.controllers.clean import EECleanController
 from ee.cli.controllers.secure import EESecureController
 from ee.cli.controllers.isl import EEImportslowlogController
 from ee.cli.controllers.info import EEInfoController
 
+
 def load(app):
     handler.register(EEBaseController)
     handler.register(EESiteController)
     handler.register(EESiteCreateController)
     handler.register(EESiteUpdateController)
-    handler.register(EEStackController)
     handler.register(EEDebugController)
     handler.register(EECleanController)
     handler.register(EEInfoController)
--- ee/cli/plugins/stack.py
+++ ee/cli/plugins/stack.py
@@ -1,6 +1,11 @@
-"""EasyEngine site controller."""
+"""Example Plugin for EasyEngine."""
 
 from cement.core.controller import CementBaseController, expose
+from cement.core import handler, hook
+
+def ee_stack_hook(app):
+    # do something with the ``app`` object here.
+    pass
 
 
 class EEStackController(CementBaseController):
@@ -36,3 +41,11 @@ class EEStackController(CementBaseController):
         # The 'default.mustache' file would be loaded from
         # ``ee.cli.templates``, or ``/var/lib/ee/templates/``.
         #
+
+
+def load(app):
+    # register the plugin class.. this only happens if the plugin is enabled
+    handler.register(EEStackController)
+
+    # register a hook (function) to run after arguments are parsed.
+    hook.register('post_argument_parsing', ee_stack_hook)
