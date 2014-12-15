e43862744a8e94631c55b1394a3bbb3d42412d95
gau1991 <gaurav.astikar@rtcamp.com>
2014-12-15 14:55:41 +0530

    Core function to download content

---
+++ ee/core/download.py
@@ -0,0 +1,24 @@
+"""EasyEngine download core classes."""
+import urllib.request
+import urllib.error
+
+
+class EEDownload():
+    """Method to download using urllib"""
+    def __init__(self):
+        pass
+
+    def download(self, url, filename):
+        try:
+            urllib.request.urlretrieve(url, filename)
+            return True
+        except urllib.error.URLError as e:
+            print("Unable to donwload file, [{err}]".format(err=str(e.reason)))
+            return False
+        except urllib.error.HTTPError as e:
+            print("Package download failed. [{err}]".format(err=str(e.reason)))
+            return False
+        except urllib.error.ContentTooShortError as e:
+            print("Package download failed. The amount of the downloaded data"
+                  "is less than the expected amount")
+            return False
