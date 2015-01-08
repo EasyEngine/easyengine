d7365dd774fb089b53e5d9518f810971c577a06a
gau1991 <gaurav.astikar@rtcamp.com>
2015-01-08 07:29:30 +0000

    Git function

--- ee/core/git.py
+++ ee/core/git.py
@@ -1,8 +1,30 @@
-"""EasyEngine GIT module"""
+from sh import git, ErrorReturnCode
+import os
 
 
 class EEGit():
     """Intialization of core variables"""
     def ___init__():
         # TODO method for core variables
+        pass
+
+    def add(paths, msg="Intializating"):
+        for path in paths:
+            agit = git.bake("--git-dir={0}/.git".format(path),
+                            "--work-tree={0}".format(path))
+            if os.path.isdir(path):
+                if not os.path.isdir(path+"/.git"):
+                    try:
+                        git.init(path)
+                    except ErrorReturnCode as e:
+                        print(e)
+                        sys.exit(1)
+                status = git.status("-s")
+                if len(status.splitlines()) > 0:
+                    try:
+                        git.add("--all")
+                        git.commit("-am {0}".format(msg))
+                    except ErrorReturnCode as e:
+                        print(e)
+                        sys.exit(1)
         pass
