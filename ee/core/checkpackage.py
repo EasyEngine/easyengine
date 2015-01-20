52e7a50283ca8d789aef0643756033b46bb67f71
gau1991 <gaurav.astikar@rtcamp.com>
2015-01-20 13:04:32 +0530

    Code clenaup

--- ee/core/checkpackage.py
+++
@@ -1,9 +0,0 @@
-"""EasyEngine package check module."""
-
-
-class EEPackageCheck():
-
-    """Intialization for package check"""
-    def ___init__():
-        # TODO Intialization for package check
-        pass
--- ee/core/swapcreation.py
+++
@@ -1,8 +0,0 @@
-"""EasyEngine swap creation module."""
-
-
-class EESwapCreation():
-    """Generice swap creation intialisation"""
-    def __init__():
-        # TODO method for swap creation
-        pass
