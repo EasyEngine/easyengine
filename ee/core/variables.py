6c4982b70834b53d6ef6c1e7cd6d22d71ef4dc2d
harshadyeola <harshad.yeola@rtcamp.com>
2015-02-03 19:54:15 +0530

    changed ee version

--- ee/core/variables.py
+++ ee/core/variables.py
@@ -12,7 +12,7 @@ class EEVariables():
     """Intialization of core variables"""
 
     # EasyEngine version
-    ee_version = "3.0.0"
+    ee_version = "3.0.0-beta"
 
     # EasyEngine packages versions
     ee_wp_cli = "0.18.0"
