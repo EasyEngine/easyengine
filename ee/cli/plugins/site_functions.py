ccd0e17e6ca02afc2879d775401ab08379f5a62b
shital.rtcamp <shital.patil@rtcamp.com>
2015-02-13 14:15:50 +0530

    updated site_function.py

--- ee/cli/plugins/site_functions.py
+++ ee/cli/plugins/site_functions.py
@@ -423,7 +423,7 @@ def site_package_check(self, stype):
         Log.debug(self, "Setting packages variable for WP-CLI")
         if not EEShellExec.cmd_exec(self, "which wp"):
             packages = packages + [["https://github.com/wp-cli/wp-cli/"
-                                    "releases/download/{0}/"
+                                    "releases/download/v{0}/"
                                     "wp-cli-{0}.phar"
                                     .format(EEVariables.ee_wp_cli),
                                     "/usr/bin/wp", "WP-CLI"]]
