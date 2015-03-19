a438448da93be7e0a0bf2666f2489b7bb1ca0341
harshadyeola <harshad.yeola@rtcamp.com>
2015-03-19 19:19:52 +0530

    updated hook name

--- ee/cli/plugins/clean.py
+++ ee/cli/plugins/clean.py
@@ -10,7 +10,7 @@ import os
 import urllib.request
 
 
-def clean_plugin_hook(app):
+def ee_clean_hook(app):
     # do something with the ``app`` object here.
     pass
 
@@ -87,4 +87,4 @@ def load(app):
     # register the plugin class.. this only happens if the plugin is enabled
     handler.register(EECleanController)
     # register a hook (function) to run after arguments are parsed.
-    hook.register('post_argument_parsing', clean_plugin_hook)
+    hook.register('post_argument_parsing', ee_clean_hook)
--- ee/cli/plugins/secure.py
+++ ee/cli/plugins/secure.py
@@ -12,7 +12,7 @@ import hashlib
 import getpass
 
 
-def secure_plugin_hook(app):
+def ee_secure_hook(app):
     # do something with the ``app`` object here.
     pass
 
@@ -139,4 +139,4 @@ def load(app):
     # register the plugin class.. this only happens if the plugin is enabled
     handler.register(EESecureController)
     # register a hook (function) to run after arguments are parsed.
-    hook.register('post_argument_parsing', secure_plugin_hook)
+    hook.register('post_argument_parsing', ee_secure_hook)
