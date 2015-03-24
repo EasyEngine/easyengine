236c8a95fc5de7416121d3d126698546abe84f8b
harshadyeola <harshad.yeola@rtcamp.com>
2015-03-24 16:45:31 +0530

    updated plugin hook names

--- ee/cli/plugins/log.py
+++ ee/cli/plugins/log.py
@@ -11,7 +11,7 @@ import os
 import glob
 
 
-def log_plugin_hook(app):
+def ee_log_hook(app):
     # do something with the ``app`` object here.
     pass
 
@@ -138,4 +138,4 @@ def load(app):
     # register the plugin class.. this only happens if the plugin is enabled
     handler.register(EELogController)
     # register a hook (function) to run after arguments are parsed.
-    hook.register('post_argument_parsing', log_plugin_hook)
+    hook.register('post_argument_parsing', ee_log_hook)
