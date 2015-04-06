9f7d54602d01571c9947ada54f0b54f11934cc24
harshadyeola <harshad.yeola@rtcamp.com>
2015-04-06 20:02:52 +0530

    fixed EEFileutils not defined

--- ee/cli/plugins/sitedb.py
+++ ee/cli/plugins/sitedb.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from ee.core.logging import Log
 from ee.core.database import db_session
+from ee.core.fileutils import EEFileUtils
 from ee.cli.plugins.models import SiteDB
 import sys
 import glob
