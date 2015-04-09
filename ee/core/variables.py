f17fcfbd073319770d7cf593fc2744cbe6ce37e4
harshadyeola <harshad.yeola@rtcamp.com>
2015-04-09 14:29:48 +0530

    fixed sync error

--- ee/core/variables.py
+++ ee/core/variables.py
@@ -12,7 +12,7 @@ class EEVariables():
     """Intialization of core variables"""
 
     # EasyEngine version
-    ee_version = "3.0.7"
+    ee_version = "3.0.8"
 
     # EasyEngine packages versions
     ee_wp_cli = "0.18.0"
--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ except Exception as e:
     os.system("git config --global user.email {0}".format(ee_email))
 
 setup(name='ee',
-      version='3.0.7',
+      version='3.0.8',
       description=long_description,
       long_description=long_description,
       classifiers=[],
