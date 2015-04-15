b2340a62e9e7db176fc2a75b416029eda9513c4f
shitalp <shital.patil@rtcamp.com>
2015-04-15 12:02:30 +0530

    minor changes

--- ee/cli/plugins/stack.py
+++ ee/cli/plugins/stack.py
@@ -1789,7 +1789,7 @@ class EEStackController(CementBaseController):
             ee_apt_pkg_prompt = input('Are you sure you to want to purge '
                                       'from server '
                                       'alongwith their configuration.'
-                                      ' packages \n Any answer other than '
+                                      '\n Any answer other than '
                                       '"yes" will be stop this '
                                       'operation :')
             if ee_apt_pkg_prompt == 'YES' or ee_apt_pkg_prompt == 'yes':
@@ -1801,7 +1801,7 @@ class EEStackController(CementBaseController):
             ee_pkg_prompt = input('Are you sure you to want to purge '
                                   ' from server alongwith '
                                   ' their configuration.'
-                                  ' packages \n Any answer other than '
+                                  '\n Any answer other than '
                                   '"yes" will be stop this '
                                   'operation :')
 
