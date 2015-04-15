fcfae907832f601cf5f027f3480401fbc074744f
shitalp <shital.patil@rtcamp.com>
2015-04-15 11:52:58 +0530

    Changed message

--- ee/cli/plugins/stack.py
+++ ee/cli/plugins/stack.py
@@ -1670,7 +1670,7 @@ class EEStackController(CementBaseController):
             ee_apt_pkg_prompt = input('Are you sure you to want to'
                                       ' purge  from server'
                                       '. Package configuration will remain'
-                                      ' on server after this operation.'
+                                      ' on server after this operation. \n'
                                       ' Any answer other than '
                                       ' "yes" will be stop this'
                                       ' operation :  ')
@@ -1684,7 +1684,7 @@ class EEStackController(CementBaseController):
             ee_pkg_prompt = input('Are you sure you to want to purge'
                                   ' from server.'
                                   ' Package configuration will remain '
-                                  'on server after this operation.'
+                                  'on server after this operation.\n'
                                   ' Any answer other than '
                                   '"yes" will be stop this '
                                   'operation : ')
@@ -1789,7 +1789,7 @@ class EEStackController(CementBaseController):
             ee_apt_pkg_prompt = input('Are you sure you to want to purge '
                                       'from server '
                                       'alongwith their configuration.'
-                                      ' packages Any answer other than '
+                                      ' packages \n Any answer other than '
                                       '"yes" will be stop this '
                                       'operation :')
             if ee_apt_pkg_prompt == 'YES' or ee_apt_pkg_prompt == 'yes':
@@ -1801,7 +1801,7 @@ class EEStackController(CementBaseController):
             ee_pkg_prompt = input('Are you sure you to want to purge '
                                   ' from server alongwith '
                                   ' their configuration.'
-                                  ' packages Any answer other than '
+                                  ' packages \n Any answer other than '
                                   '"yes" will be stop this '
                                   'operation :')
 
