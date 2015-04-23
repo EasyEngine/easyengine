1f5ce003f5f539f20ae39133744eae726a36967d
gau1991 <gaurav.astikar@rtcamp.com>
2015-04-23 16:59:28 +0530

    Added Debian 7 support for PHP 5.5 to PHP 5.6 upgrade

--- ee/cli/plugins/stack_upgrade.py
+++ ee/cli/plugins/stack_upgrade.py
@@ -52,9 +52,16 @@ class EEStackUpgradeController(CementBaseController):
 
         # PHP 5.6 to 5.6
         elif (self.app.pargs.php56):
-            if not os.path.isfile("/etc/apt/sources.list.d/"
-                                  "ondrej-php5-trusty.list"):
-                Log.error("Unable to find PHP 5.5")
+            if EEVariables.ee_platform_distro == "Ubuntu":
+                if not os.path.isfile("/etc/apt/sources.list.d/"
+                                      "ondrej-php5-trusty.list"):
+                    Log.error("Unable to find PHP 5.5")
+            else:
+                if not(os.path.isfile(EEVariables.ee_repo_file_path) and
+                       EEFileUtils.grep(self, EEVariables.ee_repo_file_path,
+                                        "php55")):
+                    Log.error("Unable to find PHP 5.5")
+
             Log.info(self, "During PHP update process non nginx-cached"
                      " parts of your site may remain down")
 
@@ -63,9 +70,16 @@ class EEStackUpgradeController(CementBaseController):
                 start_upgrade = input("Do you want to continue:[y/N]")
                 if start_upgrade != "Y" and start_upgrade != "y":
                     Log.error(self, "Not starting PHP package update")
-            EEFileUtils.remove(self, ['/etc/apt/sources.list.d/'
-                                      'ondrej-php5-trusty.list'])
-            EERepo.add(self, ppa=EEVariables.ee_php_repo)
+
+            if EEVariables.ee_platform_distro == "Ubuntu":
+                EERepo.remove(self, ppa="ppa:ondrej/php5")
+                EERepo.add(self, ppa=EEVariables.ee_php_repo)
+            else:
+                EERepo.remove(self, repo_url="deb http://packages.dotdeb.org "
+                              "{0}-php55 all"
+                              .format(EEVariables.ee_platform_codename))
+                EERepo.add(self, repo_url=EEVariables.ee_php_repo)
+
             Log.info(self, "Updating apt-cache, please wait...")
             EEAptGet.update(self)
             Log.info(self, "Installing packages, please wait ...")
