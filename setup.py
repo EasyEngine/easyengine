d6346d7cc66d2f2e6ace1c160718e1d7d0548f06
gau1991 <gaurav.astikar@rtcamp.com>
2015-06-18 17:37:42 +0530

    Version dump to 3.2.1

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ except Exception as e:
     os.system("git config --global user.email {0}".format(ee_email))
 
 setup(name='ee',
-      version='3.2.0',
+      version='3.2.1',
       description=long_description,
       long_description=long_description,
       classifiers=[],
