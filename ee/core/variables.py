5b3dac5abffc14aa64089482321e9c7079a521df
gau1991 <gaurav.astikar@rtcamp.com>
2015-07-13 18:46:39 +0530

    Version bump to 3.3.0

--- ee/core/variables.py
+++ ee/core/variables.py
@@ -12,7 +12,7 @@ class EEVariables():
     """Intialization of core variables"""
 
     # EasyEngine version
-    ee_version = "3.2.2"
+    ee_version = "3.3.0"
 
     # EasyEngine packages versions
     ee_wp_cli = "0.19.2"
