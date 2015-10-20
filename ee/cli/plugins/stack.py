395c783a91927639fdae500785e09864ab7ec3d7
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2015-10-20 15:45:41 +0530

    Updated Pin: origin

--- ee/cli/plugins/stack.py
+++ ee/cli/plugins/stack.py
@@ -104,7 +104,7 @@ class EEStackController(CementBaseController):
 
         if set(EEVariables.ee_mysql).issubset(set(apt_packages)):
             Log.info(self, "Adding repository for MySQL, please wait...")
-            mysql_pref = ("Package: *\nPin: origin mirror.aarnet.edu.au"
+            mysql_pref = ("Package: *\nPin: origin sfo1.mirrors.digitalocean.com"
                           "\nPin-Priority: 1000\n")
             with open('/etc/apt/preferences.d/'
                       'MariaDB.pref', 'w') as mysql_pref_file:
