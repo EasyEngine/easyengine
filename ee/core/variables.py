67041eb9005a2554878d4255456be15d561d7a27
harshadyeola <harshad.yeola@rtcamp.com>
2015-11-29 22:33:57 +0530

    updated Version

--- ee/core/variables.py
+++ ee/core/variables.py
@@ -12,7 +12,7 @@ class EEVariables():
     """Intialization of core variables"""
 
     # EasyEngine version
-    ee_version = "3.3.13"
+    ee_version = "3.3.14"
 
 
     # EasyEngine packages versions
