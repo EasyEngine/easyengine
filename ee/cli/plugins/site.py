013acf2abebe8d7f6f49d0cac294faaf58e4fb83
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2015-12-17 14:42:58 +0530

    added -le alias name

--- ee/cli/plugins/site.py
+++ ee/cli/plugins/site.py
@@ -769,7 +769,7 @@ class EESiteUpdateController(CementBaseController):
                 dict(help='Use PageSpeed for site',
                      action='store' or 'store_const',
                      choices=('on', 'off'), const='on', nargs='?')),
-            (['--letsencrypt'],
+            (['-le','--letsencrypt'],
                 dict(help="configure letsencrypt ssl for the site",
                      action='store' or 'store_const',
                      choices=('on', 'off', 'renew'), const='on', nargs='?')),
