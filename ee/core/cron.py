b5e53377eb88e28e6572a48adb5ba8104fb34b0d
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2015-12-18 19:48:04 +0530

    Fix issue

--- ee/core/cron.py
+++ ee/core/cron.py
@@ -8,7 +8,7 @@ Set CRON on LINUX system.
 
 class EECron():
     def setcron_daily(self,cmd,comment='Cron set by EasyEngine',user='root',min=0,hour=12):
-        if not self.check_isexist(self,cmd):
+        if not EEShellExec.cmd_exec(self, "crontab -l | grep -q \'{0}\'".format(cmd)):
             tab = CronTab(user=user)
             cron_job = tab.new(cmd, comment=comment)
             cron_job.minute().on(min)
@@ -16,12 +16,5 @@ class EECron():
             #writes to crontab
             tab.write()
             Log.debug(self, "Cron is set:\n" + tab.render())
-
-    #Check if cron already exist
-    def check_isexist(self,cmd):
-
-         if EEShellExec.cmd_exec(self, "crontab -l | grep -q \'{0}\'".format(cmd)):
-             Log.debug(self, "Cron already exist")
-             return True
-         else:
-             return False
+        else:
+            Log.debug(self, "Cron already exist")
