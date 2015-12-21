83f3015238b6a733cbbe5c4d0ed2ea798063e058
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2015-12-21 20:06:49 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -87,7 +87,6 @@ setup(name='ee',
           'psutil == 3.1.1',
           'sh',
           'sqlalchemy',
-          'python-crontab == 1.9'
           ],
       data_files=[('/etc/ee', ['config/ee.conf']),
                   ('/etc/ee/plugins.d', conf),
