082220346aca65b75641b16c3e917498b70f75ac
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2015-12-29 19:53:42 +0530

    added beta versions

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ if not os.path.isfile('/root/.gitconfig'):
       shutil.copy2(os.path.expanduser("~")+'/.gitconfig', '/root/.gitconfig')
 
 setup(name='ee',
-      version='3.4.0',
+      version='3.3.16',
       description=long_description,
       long_description=long_description,
       classifiers=[],
