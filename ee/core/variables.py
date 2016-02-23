ac9eccc3554242fb622516b9d649f27432ed062f
prabuddha <prabuddha.chakraborty@rtcamp.com>
2016-02-24 03:00:27 +0530

    php-common

--- ee/core/variables.py
+++ ee/core/variables.py
@@ -121,12 +121,12 @@ class EEVariables():
         elif ee_platform_codename == 'trusty':
             ee_php_repo = "ppa:ondrej/php"
             ee_php5_6 = ["php5.6-fpm", "php5.6-curl", "php5.6-gd", "php5.6-imap",
-                        "php5.6-mcrypt", "php5.6-readline", "php5.6-zlib",
+                        "php5.6-mcrypt", "php5.6-readline", "php5.6-common",
                         "php5.6-mysql", "php5.6-cli"]
             ee_php7_0 = ["php7.0-fpm", "php7.0-curl", "php7.0-gd", "php7.0-imap",
-                          "php7.0-mcrypt", "php7.0-readline", "php7.0-zlib",
+                          "php7.0-mcrypt", "php7.0-readline", "php7.0-common",
                           "php7.0-mysql", "php7.0-cli"]
-            ee_php_extra = ["php5.6-common", "php7.0-common", "php-memcached", "php-imagick", "memcached",
+            ee_php_extra = ["php-memcached", "php-imagick", "memcached",
                             "graphviz", "php-pear", "php-xdebug"]
     elif ee_platform_distro == 'debian':
         if ee_platform_codename == 'wheezy':
