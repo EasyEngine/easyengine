8a79f10a2c2e144e00b91d089d357085b66e2ba8
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-02-25 16:07:11 +0530

    Fix missing packages

--- ee/core/variables.py
+++ ee/core/variables.py
@@ -121,11 +121,13 @@ class EEVariables():
         elif ee_platform_codename == 'trusty':
             ee_php_repo = "ppa:ondrej/php"
             ee_php5_6 = ["php5.6-fpm", "php5.6-curl", "php5.6-gd", "php5.6-imap",
-                        "php5.6-mcrypt", "php5.6-readline", "php5.6-common",
-                        "php5.6-mysql", "php5.6-cli", "php5.6-zip", "php5.6-xml"]
+                        "php5.6-mcrypt", "php5.6-readline", "php5.6-common", "php5.6-recode",
+                        "php5.6-mysql", "php5.6-cli", "php5.6-curl", "php5.6-mbstring",
+                         "php5.6-bcmath", "php5.6-mysql", "php5.6-opcache", "php5.6-zip", "php5.6-xml"]
             ee_php7_0 = ["php7.0-fpm", "php7.0-curl", "php7.0-gd", "php7.0-imap",
-                          "php7.0-mcrypt", "php7.0-readline", "php7.0-common",
-                          "php7.0-mysql", "php7.0-cli", "php7.0-zip", "php7.0-xml"]
+                          "php7.0-mcrypt", "php7.0-readline", "php7.0-common", "php7.0-recode",
+                          "php7.0-mysql", "php7.0-cli", "php7.0-curl", "php7.0-mbstring",
+                         "php7.0-bcmath", "php7.0-mysql", "php7.0-opcache", "php7.0-zip", "php7.0-xml"]
             ee_php_extra = ["php-memcached", "php-imagick", "memcached",
                             "graphviz", "php-pear", "php-xdebug", "php-msgpack"]
     elif ee_platform_distro == 'debian':
