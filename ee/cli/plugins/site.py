4d42e5d2144705cc9e18c2116ab55ed55122c34d
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-02-25 17:40:06 +0530

    Fix reference e

--- ee/cli/plugins/site.py
+++ ee/cli/plugins/site.py
@@ -623,7 +623,6 @@ class EESiteCreateController(CementBaseController):
                     Log.info(self, Log.FAIL + "Calling cleanup actions ...")
                     doCleanupAction(self, domain=ee_domain)
                     deleteSiteInfo(self, ee_domain)
-                    Log.debug(self, str(e))
                     Log.error(self, "service nginx reload failed. "
                               "check issues with `nginx -t` command")
                     Log.error(self, "Check logs for reason "
