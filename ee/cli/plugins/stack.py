788d9b7ab5c859eb1931e542adeb6e69a7a1b3b8
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-05-25 18:32:41 +0530

    added comment

--- ee/cli/plugins/stack.py
+++ ee/cli/plugins/stack.py
@@ -2722,7 +2722,7 @@ class EEStackController(CementBaseController):
                     apt_packages = apt_packages + EEVariables.ee_php_extra
             else:
                 apt_packages = apt_packages + EEVariables.ee_php
-                
+
         #For debian --php7
         if self.app.pargs.php7:
             if (EEVariables.ee_platform_codename == 'jessie'):
@@ -2817,6 +2817,7 @@ class EEStackController(CementBaseController):
 
                 Log.info(self, "Successfully purged packages")
 
+                #Added for php Ondrej repo missing package fix
                 if self.app.pargs.php7:
                     if EEAptGet.is_installed(self, 'php5.6-fpm'):
                         Log.info(self, "PHP5.6-fpm found on system.")
