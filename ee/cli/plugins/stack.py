9d92884170d405033ac051282ddb2b94d48179f8
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-05-31 19:45:54 +0530

    xdebug on debian php7

--- ee/cli/plugins/stack.py
+++ ee/cli/plugins/stack.py
@@ -1237,8 +1237,8 @@ class EEStackController(CementBaseController):
                                  "profiler_enable] = off\n")
 
                 # Disable xdebug
-                if not EEShellExec.cmd_exec(self, "grep -q \';zend_extension\' /etc/php/7.0/mods-available/xdebug.ini"):
-                    EEFileUtils.searchreplace(self, "/etc/php/7.0/mods-available/"
+                if not EEShellExec.cmd_exec(self, "grep -q \';zend_extension\' /etc/php/mods-available/xdebug.ini"):
+                    EEFileUtils.searchreplace(self, "/etc/php/mods-available/"
                                           "xdebug.ini",
                                           "zend_extension",
                                           ";zend_extension")
