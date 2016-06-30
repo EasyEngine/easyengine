437177974dbe60f75bf0a04c6f15d40312e9e108
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-06-30 14:32:50 +0530

    Update variable

--- ee/core/variables.py
+++ ee/core/variables.py
@@ -12,8 +12,7 @@ class EEVariables():
     """Intialization of core variables"""
 
     # EasyEngine version
-    ee_version = "3.7.0"
-
+    ee_version = "3.7.1"
     # EasyEngine packages versions
     ee_wp_cli = "0.23.0"
     ee_adminer = "4.2.5"
