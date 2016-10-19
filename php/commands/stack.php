7f390e3cc3ffa3fb5cfe185775c2aba2894707a4
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-10-19 19:04:06 +0530

    Adding php packages on stack install command

--- php/commands/stack.php
+++ php/commands/stack.php
@@ -1,5 +1,8 @@
 <?php
 
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
 
 /**
  * Manage sites.
@@ -45,19 +48,114 @@ class Stack_Command extends EE_Command {
 	 */
 	public function install( $args, $assoc_args ) {
 
-		if( ! empty( $assoc_args['php'] ) ) {
-			$phpbi = EE::get_config();
-			print_r( $phpbi );
+		list( $site_name ) = $args;
+
+		if( !empty( $assoc_args['pagespeed'] ) ) {
+			EE_CLI::error( $site_name . 'Pagespeed support has been dropped since EasyEngine v3.6.0' );
+			EE_CLI::error( $site_name . 'Please run command again without `--pagespeed`' );
+			EE_CLI::error( $site_name . 'For more details, read - https://easyengine.io/blog/disabling-pagespeed/' );
 		}
-		if ( ! empty( $assoc_args['nginx'] ) ) {
-			$check_nginx = EE::exec_cmd( 'nginx -t', 'Checking nginx..' );
-			if ( 0 == $check_nginx ) {
-				EE::success( 'Nginx is already available' );
-			} else {
-				EE::success( 'Please install nginx.' );
+
+	if(!empty( $assoc_args['all'] )){
+		$category['web'] = True;
+		$category['admin'] = True;
+	}
+	$stack = array();
+	if ($category['web'] == true){
+			$stack['nginx']= true;
+			$stack['php']= true;
+			$stack['mysql']= true;
+			$stack['wpcli']= true;
+			$stack['postfix']= true;
+	}
+	if ($category['admin'] == true){
+		$stack['nginx']= true;
+		$stack['php']= true;
+		$stack['mysql']= true;
+		$stack['adminer']= true;
+		$stack['phpmyadmin']= true;
+		$category['utils']= true;
+	}
+
+	// if ($category['mail'] == true){
+	// todo:
+	// }
+
+	if (!empty($stack['redis'])){
+		if(!EE_Apt_Get::is_installed('redis-server')){
+			$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('redis'));
+	}
+	}else{
+			EE::success("Redis already installed");
+		}
+	}
+
+	if (!empty($stack['nginx'])){
+		EE::debug("Setting apt_packages variable for Nginx");
+		if(!EE_Apt_Get::is_installed('nginx-custom')){
+			if(!(EE_Apt_Get::is_installed('nginx-plus')||EE_Apt_Get::is_installed('nginx'))){
+				$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('nginx'));
+			}else{
+					if(EE_Apt_Get::is_installed('nginx-plus')){
+						EE::success("NGINX PLUS Detected ...");
+						$apt[]="nginx-plus";
+						$apt=array_merge($apt,EE_Variables::get_package_list('nginx'));
+						self::post_pref($apt, $packages);
+					}elseif(EE_Apt_Get::is_installed('nginx')){
+						EE:success("EasyEngine detected a previously installed Nginx package. ".
+						"It may or may not have required modules. ".
+						"\nIf you need help, please create an issue at https://github.com/EasyEngine/easyengine/issues/ \n");
+						$apt[]="nginx";
+						$apt=array_merge($apt,EE_Variables::get_package_list('nginx'));
+						self::post_pref($apt, $packages);
+					}
+			}
+		}else{
+			EE::debug("Nginx Stable already installed");
+		}
+	}
+	if (!empty($stack['php'])){
+		EE::debug("Setting apt_packages variable for PHP");
+		if(!(EE_Apt_Get::is_installed('php5-fpm')||if(EE_Apt_Get::is_installed('php5.6-fpm')))){
+			if(EE_OS::ee_platform_codename() == 'trusty'||EE_OS::ee_platform_codename() == 'xenial'){
+				$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php5.6'),EE_Variables::get_package_list('phpextra'));
+			}else{
+				$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php'));
+			}
+		}else{
+			EE:success("PHP already installed");
+		}
+	}
+
+	if (!empty($stack['php'] && EE_OS::ee_platform_distro == 'debian')){
+		if (EE_OS::ee_platform_codename == 'jessie'){
+			EE:debug("Setting apt_packages variable for PHP 7.0");
+			if(!EE_Apt_Get::is_installed('php7.0-fpm')){
+				$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php7.0'));
+				if(!EE_Apt_Get::is_installed('php5-fpm')){
+					$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php'));
+				}
+			}else{
+				EE::success("PHP 7.0 already installed");
 			}
 		}
 	}
-}
+
+
+	if (!empty($stack['php'] && !EE_OS::ee_platform_distro == 'debian')){
+		if (EE_OS::ee_platform_codename == 'trusty'||EE_OS::ee_platform_codename == 'xenial'){
+			EE:debug("Setting apt_packages variable for PHP 7.0");
+			if(!EE_Apt_Get::is_installed('php7.0-fpm')){
+				$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php7.0'));
+				if(!EE_Apt_Get::is_installed('php5.6-fpm')){
+					$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php5.6'),EE_Variables::get_package_list('phpextra'));
+				}
+			}else{
+				EE::success("PHP 7.0 already installed");
+			}
+		}
+	}
+
+
 
 EE::add_command( 'stack', 'Stack_Command' );
