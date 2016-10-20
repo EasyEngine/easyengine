8625c0dbf6a066c78d36b0fbacda6d9934d4174e
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-10-20 16:24:02 +0530

    Moving random_string() to ee-utils

--- php/utils.php
+++ php/utils.php
@@ -368,22 +368,6 @@ function get_flag_value( $args, $flag, $default = null ) {
 }
 
 /**
- * Generate random string.
- *
- * @return string
- */
-function randomString($length = 6) {
-	$str = "";
-	$characters = array_merge(range('A','Z'), range('a','z'), range('0','9'));
-	$max = count($characters) - 1;
-	for ($i = 0; $i < $length; $i++) {
-		$rand = mt_rand(0, $max);
-		$str .= $characters[$rand];
-	}
-	return $str;
-}
-
-/**
  * Get the temp directory, and let the user know if it isn't writable.
  *
  * @return string
--- php/utils/class-ee-utils.php
+++ php/utils/class-ee-utils.php
@@ -82,4 +82,20 @@ class EE_Utils {
 
 		return $url;
 	}
+
+	/**
+	 * Generate random string.
+	 *
+	 * @return string
+	 */
+	function random_string($length = 6) {
+		$str = "";
+		$characters = array_merge(range('A','Z'), range('a','z'), range('0','9'));
+		$max = count($characters) - 1;
+		for ($i = 0; $i < $length; $i++) {
+			$rand = mt_rand(0, $max);
+			$str .= $characters[$rand];
+		}
+		return $str;
+	}
 }
