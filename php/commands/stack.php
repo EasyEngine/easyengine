bee4e938f7608aae7b5d98f02021c64a2d07949e
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-10-26 14:33:32 +0530

    Added stack purge command

--- php/commands/stack.php
+++ php/commands/stack.php
@@ -968,7 +968,178 @@ class Stack_Command extends EE_Command {
 		}
 	}
 
+	public function remove( $args, $assoc_args ) {
 
+		list( $site_name ) = $args;
+		$apt_packages = array();
+		$packages = array();
+		$stack = array();
+		if(!empty($assoc_args['nginx'])){
+			$stack['nginx']= true;
+		}
+
+		if (!empty($assoc_args['no_diplay_message'])){
+			$disp_msg = false;
+		}else{
+			$disp_msg = true;
+		}
+
+		if( !empty( $assoc_args['pagespeed'] ) ) {
+			EE::error( $site_name . 'Pagespeed support has been dropped since EasyEngine v3.6.0' );
+			EE::error( $site_name . 'Please run command again without `--pagespeed`' );
+			EE::error( $site_name . 'For more details, read - https://easyengine.io/blog/disabling-pagespeed/' );
+		}
+
+		if(!empty( $assoc_args['all'] )){
+			$category['web'] = True;
+			$category['admin'] = True;
+		}
+
+		if ($category['web'] == true){
+			$stack['nginx']= true;
+			$stack['php']= true;
+			$stack['mysql']= true;
+			$stack['wpcli']= true;
+			$stack['postfix']= true;
+		}
+		if ($category['admin'] == true){
+			$stack['nginx']= true;
+			$stack['php']= true;
+			$stack['mysql']= true;
+			$stack['adminer']= true;
+			$stack['phpmyadmin']= true;
+			$category['utils']= true;
+		}
+
+		// if ($category['mail'] == true){
+		// todo:
+		// }
+
+		if (!empty($stack['redis'])) {
+			if (  EE_Apt_Get::is_installed( 'redis-server' ) ) {
+
+				$apt_packages = array_merge( $apt_packages, EE_Variables::get_redis_packages() );
+			} else {
+				EE::success( "Redis not installed" );
+			}
+		}
+
+		if ($stack['nginx']){
+			if(EE_Apt_Get::is_installed('nginx-custom')){
+
+				$apt_packages=array_merge($apt_packages,EE_Variables::get_nginx_packages());
+			}else{
+				EE::log("Nginx Stable not installed");
+			}
+		}
+		if (!empty($stack['php'])){
+			EE::debug("Setting apt_packages variable for PHP");
+			if(EE_Apt_Get::is_installed('php5-fpm')||EE_Apt_Get::is_installed('php5.6-fpm')){
+				if(EE_OS::ee_platform_codename() == 'trusty'||EE_OS::ee_platform_codename() == 'xenial'){
+					$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php5.6'),EE_Variables::get_package_list('phpextra'));
+				}else{
+					$apt_packages = array_merge($apt_packages,EE_Variables::get_php_packages( 'php' ));
+				}
+			}else{
+				EE::success("PHP not installed");
+			}
+		}
+
+		if ( EE_OS::ee_platform_distro() == 'debian' && !empty($stack['php'])){
+			if (EE_OS::ee_platform_codename() == 'jessie'){
+				EE::debug("Setting apt_packages variable for PHP 7.0");
+				if(EE_Apt_Get::is_installed('php7.0-fpm')){
+					$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php7.0'));
+					if(EE_Apt_Get::is_installed('php5-fpm')){
+						$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php'));
+					}
+				}else{
+					EE::success("PHP 7.0 not installed");
+				}
+			}
+		}
+
+
+		if (!empty($stack['php'] && !EE_OS::ee_platform_codename() == 'debian')){
+			if (EE_OS::ee_platform_codename() == 'trusty'||EE_OS::ee_platform_codename() == 'xenial'){
+				EE::debug("Setting apt_packages variable for PHP 7.0");
+				if(EE_Apt_Get::is_installed('php7.0-fpm')){
+					$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php7.0'));
+					if(EE_Apt_Get::is_installed('php5.6-fpm')){
+						$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('php5.6'),EE_Variables::get_package_list('phpextra'));
+					}
+				}else{
+					EE::success("PHP 7.0 not installed");
+				}
+			}
+		}
+
+		if (!empty($stack['mysql'])){
+			EE::debug("Setting apt_packages variable for MySQL");
+			if (EE::exec_cmd("mysqladmin ping", $message = 'Looking for active mysql connection')){
+				$apt_packages = array_merge($apt_packages,EE_Variables::get_mysql_packages());
+				$packages = array_merge($packages, array("mysqltunner"));
+			}else{
+				EE::success("MySQL connection is not alive");
+			}
+		}
+
+
+		if (!empty($stack['postfix'])){
+			EE::debug("Setting apt_packages variable for Postfix");
+			if(EE_Apt_Get::is_installed('postfix')){
+				$apt_packages = array_merge($apt_packages,EE_Variables::get_package_list('postfix'));
+			}else{
+				EE::success("Postfix is not installed");
+			}
+		}
+
+		if (!empty($stack['wpcli'])){
+			EE::debug("Setting packages variable for WP-CLI");
+			if (EE::exec_cmd("which wp", $message = 'Looking wp-cli preinstalled')){
+				$packages = array_merge($packages, array("wpcli"));
+			}
+			else{
+				EE::success("WP-CLI is not installed");
+			}
+		}
+
+		if (!empty($stack['phpmyadmin'])){
+			EE::debug("Setting packages variable for phpMyAdmin");
+			$packages = array_merge($packages, array("phpmyadmin"));
+		}
+
+		if (!empty($stack['phpredisadmin'])){
+			EE::debug("Setting packages variable for phpRedisAdmin");
+			$packages = array_merge($packages, array("phpredisadmin"));
+		}
+
+		if (!empty($stack['adminer'])){
+			EE::debug("Setting packages variable for Adminer");
+			$packages = array_merge($packages, array("adminer"));
+		}
+
+		if (!empty($category['utils'])){
+			EE::debug("Setting packages variable for utils");
+			$packages = array_merge($packages, array("phpmemcacheadmin","opcache","rtcache-clean", "opcache-gui","ocp","webgrind","perconna-toolkit","anemometer"));
+		}
+
+		if(!empty($apt_packages)||!empty($packages)){;
+			if(!empty($apt_packages)){
+				EE_Apt_Get::remove($apt_packages,true);
+			}
+			if(!empty($packages)){
+				EE::debug("Removing following: " .implode(' ',$packages));
+				EE_Utils::remove($packages);
+			}
+
+			if ($disp_msg){
+				EE::success("Successfully Removed Packages");
+			}
+
+
+		}
+	}
 
 }
 
