6decec7a2001a4debe7f283cbe1dd910afdc5b23
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-10-27 05:17:13 +0530

    defined EE constant variables

--- php/ee-config.php
+++ php/ee-config.php
@@ -10,3 +10,7 @@ define( 'EE_DB_FILE', '/var/lib/ee/ee.db' );
 define( 'EE_WEBROOT', '/var/www/');
 //define( 'EE_CONFIG_FILE', '/etc/ee/ee.conf' );
 define( 'EE_CONFIG_FILE', EE_ROOT . '/config/ee.conf' );
+define( 'EE_ADMINER', '4.2.5');
+define('EE_ROUNDCUBE', '1.1.4');
+define('EE_VIMBADMIN', 'master');
+
