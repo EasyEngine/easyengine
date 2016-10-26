d305dd86ce96d74602d487ea593a455c2f01b8a1
Prabuddha Chakraborty <prabuddha.chakraborty@rtcamp.com>
2016-10-26 14:26:44 +0530

    added is_installed functionality

--- php/utils/class-ee-apt-get.php
+++ php/utils/class-ee-apt-get.php
@@ -142,7 +142,12 @@ class EE_Apt_Get
 	}
 
  	public static function is_installed($package){
- 		//todo:
+ 		if(!EE::exec_cmd("dpkg -s ".$package." | grep Status")){
+		    return true;
+	    }else{
+		    return false;
+	    }
+
  }
 }
 
