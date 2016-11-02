8af98e7142a771489a5933c3e84059525190d662
Prabuddha <prabuddha.chakraborty@rtcamp.com>
2016-11-02 14:27:16 +0530

    update travis.yml

--- php/commands/debug.php
+++ php/commands/debug.php
@@ -6,37 +6,42 @@ use \EE\Dispatcher;
 class debug_Command extends EE_Command {
 
 
-public function debug_nginx($_argc){
+	public function debug_nginx($_argc){
 //	"""Start/Stop Nginx debug"""
 
-	$debug = $_argc;
-	$debug_address = '0.0.0.0/0';
+		$debug = $_argc;
+		$debug_address = '0.0.0.0/0';
 
-	if('on'=== $debug['nginx'] && empty($debug['sitename'])){
-		if(!grep_string('/etc/nginx/nginx.conf','debug_connection')){
-			EE::success("Setting up Nginx debug connection for 0.0.0.0/0");
-			EE::exec_cmd("sed -i \"/events {{/a\\ \\ \\ \\ $(echo debug_connection ".$debug_address.";)\" /etc/nginx/nginx.conf");
-		}else{
-			EE::success("Nginx debug connection already enabled");
-		}
+		if('on'=== $debug['nginx'] && empty($debug['sitename'])){
+			if(!grep_string('/etc/nginx/nginx.conf','debug_connection')){
+				EE::success("Setting up Nginx debug connection for 0.0.0.0/0");
+				EE::exec_cmd("sed -i \"/events {{/a\\ \\ \\ \\ $(echo debug_connection ".$debug_address.";)\" /etc/nginx/nginx.conf");
+			}else{
+				EE::success("Nginx debug connection already enabled");
+			}
 
-	}elseif('off'=== $debug['nginx'] && empty($debug['sitename'])){
-		if(grep_string('/etc/nginx/nginx.conf','debug_connection')){
-			EE::success("Disabling Nginx debug connections");
-			EE::exec_cmd("sed -i \"/debug_connection.*/d\" /etc/nginx/nginx.conf");
-		}else{
-			EE::success("Nginx debug connection already disabled");
-		}
-	}elseif('on'=== $debug['nginx'] && !empty($debug['sitename'])){
-
-		//todo:
-
-	}elseif('off'=== $debug['nginx'] && !empty($debug['sitename'])){
+		}elseif('off'=== $debug['nginx'] && empty($debug['sitename'])){
+			if(grep_string('/etc/nginx/nginx.conf','debug_connection')){
+				EE::success("Disabling Nginx debug connections");
+				EE::exec_cmd("sed -i \"/debug_connection.*/d\" /etc/nginx/nginx.conf");
+			}else{
+				EE::success("Nginx debug connection already disabled");
+			}
+		}elseif('on'=== $debug['nginx'] && !empty($debug['sitename'])){
 
 			//todo:
+
+		}elseif('off'=== $debug['nginx'] && !empty($debug['sitename'])){
+
+				//todo:
+		}
+
 	}
 
-}
+	public function debug_php(){
+		// """Start/Stop PHP debug"""
+
+	}
 
 }
 
