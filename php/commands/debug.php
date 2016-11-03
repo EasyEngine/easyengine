b9f70a328bc0054a44c3695d1d8e55d7e67e5d14
Prabuddha <prabuddha.chakraborty@rtcamp.com>
2016-11-03 16:40:52 +0530

    Added wp debug feature

--- php/commands/debug.php
+++ php/commands/debug.php
@@ -74,7 +74,55 @@ class debug_Command extends EE_Command {
 
 	public function debug_wp( $_argc ) {
 		//Start/Stop WordPress debug
-		//todo://
+
+		$debug = $_argc;
+		if ( 'on' === $debug['wp'] && ! empty( $debug['sitename'] ) ) {
+			$wp_config = EE_WEBROOT . $debug['sitename'] . "/wp-config.php";
+			$webroot   = EE_WEBROOT . $debug['sitename'];
+
+			if ( ! is_file( $wp_config ) ) {
+				$wp_config = EE_WEBROOT . $debug['sitename'] . "/htdocs/wp-config.php";
+			}
+
+			if ( ! is_file( $wp_config ) ) {
+				if ( ! EE::exec_cmd( "grep \"'WP_DEBUG'\" " . $wp_config . " | grep true" ) ) {
+					EE::success( "Starting WordPress debug" );
+					ee_file_touch( $webroot . "/htdocs/wp-content/debug.log" );
+					ee_file_chown( $webroot . "/htdocs/wp-content/debug.log", EE_PHP_USER );
+					EE::exec_cmd( "sed -i \"s/define('WP_DEBUG'.*/define('WP_DEBUG', true);
+																\ndefine('WP_DEBUG_DISPLAY', false);
+																\ndefine('WP_DEBUG_LOG', true);
+																\ndefine('SAVEQUERIES', true);/\" " . $wp_config . "" );
+					EE::exec_cmd( "cd " . $webroot . "/htdocs/ && wp plugin --allow-root install developer query-monitor" );
+					EE::exec_cmd( "chown -R " . EE_PHP_USER . ":" . " " . $webroot . "/htdocs/wp-content/plugins" );
+					EE::success( "Log Enabled: " . $webroot . "/htdocs/wp-content/debug.log" );
+				}
+			} else {
+				EE::error( "Unable to find wp-config.php for site", false );
+			}
+		} elseif ( 'off' === $debug['wp'] && ! empty( $debug['sitename'] ) ) {
+			$wp_config = EE_WEBROOT . $debug['sitename'] . "/wp-config.php";
+			$webroot   = EE_WEBROOT . $debug['sitename'];
+
+			if ( ! is_file( $wp_config ) ) {
+				$wp_config = EE_WEBROOT . $debug['sitename'] . "/htdocs/wp-config.php";
+			}
+
+			if ( ! is_file( $wp_config ) ) {
+				if ( EE::exec_cmd( "grep \"'WP_DEBUG'\" " . $wp_config . " | grep true" ) ) {
+					EE::success( "Disabling WordPress debug" );
+					EE::exec_cmd( "sed -i \"s/define('WP_DEBUG', true);/define('WP_DEBUG', false);/\" " . $wp_config . "" );
+					EE::exec_cmd( "sed -i \"/define('WP_DEBUG_DISPLAY', false);/d\" " . $wp_config . "" );
+					EE::exec_cmd( "sed -i \"/define('WP_DEBUG_LOG', true);/d\" " . $wp_config . "" );
+					EE::exec_cmd( "sed -i \"/define('SAVEQUERIES', true);/d\" " . $wp_config . "" );
+				} else {
+					EE::error( "WordPress debug already disabled", false );
+				}
+			}
+
+		} else {
+			EE::error( "Missing argument site name" );
+		}
 	}
 
 }
