8752136fd39285ffb63f48df4e61f0ae914fb60d
Prabuddha <prabuddha.chakraborty@rtcamp.com>
2016-11-07 18:57:39 +0530

    Fix site cd command

--- php/commands/site.php
+++ php/commands/site.php
@@ -1033,6 +1033,7 @@ class Site_Command extends EE_Command {
 
 		try {
 			chdir( $ee_site_webroot );
+			exec("bash");
 		} catch ( Exception $e ) {
 			EE::debug( $e->getMessage() );
 			EE::error( "unable to change directory" );
