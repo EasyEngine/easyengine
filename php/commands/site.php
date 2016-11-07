e888175dc7e064d497511f325211c2e24283ade1
Prabuddha <prabuddha.chakraborty@rtcamp.com>
2016-11-07 21:29:34 +0530

    move to sec

--- php/commands/site.php
+++ php/commands/site.php
@@ -1033,7 +1033,7 @@ class Site_Command extends EE_Command {
 
 		try {
 			chdir( $ee_site_webroot );
-			system("bash");
+			exec("bash > /dev/tty");
 		} catch ( Exception $e ) {
 			EE::debug( $e->getMessage() );
 			EE::error( "unable to change directory" );
