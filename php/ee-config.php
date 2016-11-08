e81147ee4d0abfd3d790ab3af67d91d6cd0ac789
Prabuddha <prabuddha.chakraborty@rtcamp.com>
2016-11-08 11:52:19 +0530

    Added debug file

--- php/ee-config.php
+++ php/ee-config.php
@@ -2,7 +2,7 @@
 /**
  * Define constant variables for ee.
  */
-define( 'EE_DEBUG_LOG_FILE', EE_ROOT . '/testlog.log' );
+define( 'EE_DEBUG_LOG_FILE', EE_ROOT . '/var/log/ee.log' );
 define( 'EE_WP_CLI', '0.24.1' );
 define( 'EE_NGINX_SITE_AVAIL_DIR', '/etc/nginx/sites-available/' );
 define( 'EE_NGINX_SITE_ENABLE_DIR', '/etc/nginx/sites-enabled/' );
