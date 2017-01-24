7cc8f8f539be29f0dbdc264613c83d020ef5d438
Bharat <bharat.parsiya@rtcamp.com>
2017-01-24 11:13:52 +0530

    Added debuging  logs to command

--- php/commands/secure.php
+++ php/commands/secure.php
@@ -39,19 +39,30 @@ class Secure_Command extends EE_Command {
 		EE::info("Please Enter valid port number");
 		$port = EE::input_value("EasyEngine admin port [22222]:");
 		if(empty($port)){
+			EE::debug("No port selected using default port : 22222");
 			$port = 22222;
 		}
+		
 		while(!is_numeric($port)){
+			EE::debug("Not a valid port");
 			EE::error("Please enter valid port number:");
 			$port = EE::input_value("EasyEngine admin port [22222]:");
 		}
+		EE::debug("Checking distro of the machine");
 		$distro_name = EE_OS::ee_platform_distro();
+		EE::debug("$distro_name found");
 		if("ubuntu" == $distro_name) {
+			EE::debug("Changing nginx configuration");
 			EE::exec_cmd("sed -i \"s/listen.*/listen $port default_server ssl http2;/\" /etc/nginx/sites-available/22222");
 		} else if ( "debian" == $distro_name){
+			EE::debug("Changing nginx configuration");
 			EE::exec_cmd("sed -i \"s/listen.*/listen $port default_server ssl http2;/\" /etc/nginx/sites-available/22222");
+		} else {
+			EE::error("Unknown enviorment found");
 		}
+		EE::debug("Adding changes to git");
 		EE_Git::add("/etc/nginx","Adding changed port to git");
+		EE::debug("Restarting nginx");
 		EE_Service::restart_service("nginx");
 	}
 
@@ -64,16 +75,20 @@ class Secure_Command extends EE_Command {
 		if( !empty($ips) ){
 			foreach($ips as $index => $ip){
 				$ip = trim($ip);
+				EE::debug("Validating IP Address:$ip");
 				if (!filter_var($ip, FILTER_VALIDATE_IP) === false) {
+					EE::debug("Valid IP Address:$ip, adding it to acl.conf");
 					EE::exec_cmd("sed -i \"/deny/i allow $ip;\" /etc/nginx/common/acl.conf");
 				} else {
-					echo("$ip is not a valid IP address");
+					EE::Error("$ip is not a valid IP address. Skipping for now");
 				}
 			}
 		}
+		
 		EE_Git::add("/etc/nginx","Adding white listed to git");
+		EE::debug("Restarting nginx");
 		EE_Service::restart_service("nginx");
-		EE::log("Successfully added IP address in acl.conf file");
+		EE::log("Successfully added IP address(es) in acl.conf file");
 
 
 	}
