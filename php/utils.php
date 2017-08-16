3610c86c4d6126c0c8545b7fd31d7a0df1ec2a79
Bharat Parsiya <bnap00@gmail.com>
2017-08-16 20:50:14 +0530

    remove obsolete functions and its calls

--- php/utils.php
+++ php/utils.php
@@ -55,20 +55,6 @@ function load_command( $name ) {
 	}
 }
 
-function load_all_commands() {
-	$cmd_dir = EE_ROOT . '/php/commands/';
-
-	$iterator = new \DirectoryIterator( $cmd_dir );
-
-	foreach ( $iterator as $filename ) {
-		if ( '.php' != substr( $filename, - 4 ) ) {
-			continue;
-		}
-
-		include_once "$cmd_dir/$filename";
-	}
-}
-
 /**
  * Like array_map(), except it returns a new iterator, instead of a modified array.
  *
