3eacd1365752a598c6f9f96e98448217c88b0f9d
Kirtan Gajjar <kirtangajjar95@gmail.com>
2018-06-15 10:27:45 +0530

    Port existing changes to v3

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,64 +1,153 @@
 <?php
 
+include_once(__DIR__ . '/../../php/class-ee.php');
+include_once(__DIR__ . '/../../php/utils.php');
+
 use Behat\Behat\Context\Context;
 
 
 use Behat\Gherkin\Node\PyStringNode,
-	Behat\Gherkin\Node\TableNode,
-	Behat\Gherkin\Node\OutlineNode;
+	Behat\Gherkin\Node\TableNode;
 
-use Behat\Behat\Event\FeatureEvent;
-
-class FeatureContext implements Context {
+class FeatureContext implements Context
+{
 	public $command;
 	public $webroot_path;
-	
-	
+
 	/**
-     * @When I run :arg1
-     */
-    public function iRun($arg1)
-    {
-        throw new PendingException();
-    }
+	 * @Given :command is installed
+	 */
+	public function isInstalled($command)
+	{
+		exec("type " . $command, $output, $return_status);
+		if (0 !== $return_status) {
+			throw new Exception($command . " is not installed! Exit code is:" . $return_status);
+		}
+	}
 
-    /**
-     * @Then STDOUT should return exactly
-     */
-    public function stdoutShouldReturnExactly(PyStringNode $string)
-    {
-        throw new PendingException();
-    }
+	/**
+	 * @When I run :command
+	 */
+	public function iRun($command)
+	{
+		$this->command = EE::launch($command, false, true);
+	}
 
-    /**
-     * @Then STDERR should be empty
-     */
-    public function stderrShouldBeEmpty()
-    {
-        throw new PendingException();
-    }
+	/**
+	 * @Then /(STDOUT|STDERR) should return exactly/
+	 */
+	public function stdoutShouldReturnExactly($output_stream, PyStringNode $expected_output)
+	{
+		$command_output = $output_stream === "STDOUT" ? $this->command->stdout : $this->command->stderr;
 
-    /**
-     * @Then The :arg1 db entry should be removed
-     */
-    public function theDbEntryShouldBeRemoved($arg1)
-    {
-        throw new PendingException();
-    }
+		if ((string)$expected_output !== trim($command_output)) {
+			throw new Exception("Actual output is:\n" . $command_output);
+		}
+	}
 
-    /**
-     * @Then The :arg1 webroot should be removed
-     */
-    public function theWebrootShouldBeRemoved($arg1)
-    {
-        throw new PendingException();
-    }
+	/**
+	 * @Then /(STDOUT|STDERR) should return something like/
+	 */
+	public function stdoutShouldReturnSomethingLike($output_stream, PyStringNode $expected_output)
+	{
+		$command_output = $output_stream === "STDOUT" ? $this->command->stdout : $this->command->stderr;
 
-    /**
-     * @Then Following containers of site :arg1 should be removed:
-     */
-    public function followingContainersOfSiteShouldBeRemoved($arg1, TableNode $table)
-    {
-        throw new PendingException();
-    }
+		if (strpos($command_output, (string)$expected_output) === false) {
+			throw new Exception("Actual output is:\n" . $command_output);
+		}
+	}
+
+	/**
+	 * @Then The :site db entry should be removed
+	 */
+	public function theDbEntryShouldBeRemoved($site)
+	{
+		$out = shell_exec("sudo bin/ee site list");
+		if (strpos($out, $site) !== false) {
+			throw new Exception("$site db entry not been removed!");
+		}
+
+	}
+
+	/**
+	 * @Then The :site webroot should be removed
+	 */
+	public function theWebrootShouldBeRemoved($site)
+	{
+		if (file_exists(getenv('HOME') . "/ee-sites/" . $site)) {
+			throw new Exception("Webroot has not been removed!");
+		}
+	}
+
+	/**
+	 * @Then Following containers of site :site should be removed:
+	 */
+	public function followingContainersOfSiteShouldBeRemoved($site, TableNode $table)
+	{
+		$containers = $table->getHash();
+		$site_name = implode(explode('.', $site));
+
+		foreach ($containers as $container) {
+
+			$sevice = $container['container'];
+			$container_name = $site_name . '_' . $sevice . '_1';
+
+			exec("docker inspect -f '{{.State.Running}}' $container_name > /dev/null 2>&1", $exec_out, $return);
+			if (!$return) {
+				throw new Exception("$container_name has not been removed!");
+			}
+		}
+	}
+
+	/**
+	 * @Then The site :site should have webroot
+	 */
+	public function theSiteShouldHaveWebroot($site)
+	{
+		if (!file_exists(getenv('HOME') . "/ee-sites/" . $site)) {
+			throw new Exception("Webroot has not been created!");
+		}
+	}
+
+	/**
+	 * @Then The site :site should have WordPress
+	 */
+	public function theSiteShouldHaveWordpress($site)
+	{
+		if (!file_exists(getenv('HOME') . "/ee-sites/" . $site . "/app/src/wp-config.php")) {
+			throw new Exception("WordPress data not found!");
+		}
+	}
+
+	/**
+	 * @Then Request on :site should contain following headers:
+	 */
+	public function requestOnShouldContainFollowingHeaders($site, TableNode $table)
+	{
+		$url = 'http://' . $site;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		curl_setopt($ch, CURLOPT_NOBODY, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$headers = curl_exec($ch);
+		curl_close($ch);
+
+		$rows = $table->getHash();
+
+		foreach ($rows as $row) {
+			if (strpos($headers, $row['header']) === false) {
+				throw new Exception("Unable to find " . $row['header'] . "\nActual output is : " . $headers);
+			}
+		}
+	}
+
+	/**
+	 * @AfterFeature
+	 */
+	public static function cleanup(AfterScenarioScope $scope)
+	{
+		exec("bin/ee site delete hello.test");
+	}
 }
--- features/site.feature
+++ features/site.feature
@@ -1,47 +1,41 @@
 Feature: Site Command
 
-  # Scenario: ee throws error when run without root
-  #   Given 'bin/ee' is installed
-  #   When I run 'bin/ee'
-  #   Then STDOUT should return something like
-  #   """
-  #   Error: Please run `ee` with root privileges.
-  #   """
+  Scenario: ee throws error when run without root
+    Given 'bin/ee' is installed
+    When I run 'bin/ee'
+    Then STDERR should return exactly
+    """
+    Error: Please run `ee` with root privileges.
+    """
 
-  # Scenario: ee executable is command working correctly
-  #   Given 'bin/ee' is installed
-  #   When I run 'sudo bin/ee'
-  #   Then STDOUT should return something like
-  #    """
-  #    NAME
+  Scenario: ee executable is command working correctly
+    Given 'bin/ee' is installed
+    When I run 'sudo bin/ee'
+    Then STDOUT should return something like
+    """
+    NAME
 
-  #    ee
-  #    """
+      ee
+    """
 
-  # Scenario: Check site command is present
-  #   When I run 'sudo bin/ee site'
-  #   Then STDOUT should return something like
-  #    """
-  #     usage: ee site
-  #    """
+  Scenario: Check site command is present
+    When I run 'sudo bin/ee site'
+    Then STDOUT should return something like
+    """
+    usage: ee site
+    """
 
-  # Scenario Outline: 'site create' is running successfully
-  #   When I run 'sudo bin/ee site create <site> --wp'
-  #   Then The site '<site>' should have webroot
-  #   Then The site '<site>' should have WordPress
-  #   Then Request on '<site>' should contain following headers:
-  #   | header           |
-  #   | HTTP/1.1 200 OK  |
+  Scenario: Create site successfully
+    When I run 'sudo bin/ee site create hello.test --wp'
+    Then The site 'hello.test' should have webroot
+    And The site 'hello.test' should have WordPress
+    And Request on 'hello.test' should contain following headers:
+    | header           |
+    | HTTP/1.1 200 OK  |
 
-  #   # And The '<site>' should have tables
-
-  #   Examples:
-  #     | site       |
-  #     | hello.test |
-
- Scenario: List the sites
-   When I run 'sudo bin/ee site list'
-   Then STDOUT should return exactly
+  Scenario: List the sites
+    When I run 'sudo bin/ee site list'
+    Then STDOUT should return exactly
     """
     List of all Sites:
 
@@ -50,26 +44,19 @@ Feature: Site Command
 
   Scenario: Delete the sites
     When I run 'sudo bin/ee site delete hello.test'
-    Then STDERR should be empty
+    Then STDOUT should return something like
+    """
+    Site hello.test deleted.
+    """
+    And STDERR should return exactly
+    """
+    """
     And The 'hello.test' db entry should be removed
     And The 'hello.test' webroot should be removed
-    And Following containers of site 'hello.test' should be removed:
+    And Following containers of site 'abc.test' should be removed:
       | container  |
       | nginx      |
       | php        |
       | db         |
       | redis      |
       | phpmyadmin |
-
-
-
-#Scenario: Site Clean-up works properly
-#    When I cleanup test 'sudo bin/ee site create <site>'
-#    Then The '<site>' containers should be removed
-#    And The '<site>' webroot should be removed
-#    And The '<site>' db entry should be removed
-#
-#    Examples:
-#      | site       |
-#      | hello.test |
-#      | world.test |
