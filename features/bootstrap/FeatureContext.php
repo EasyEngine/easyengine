20c1f19f8d51aa3862bfd3f2b9364a7be973561c
Riddhesh Sanghvi <riddhesh.sanghvi@rtcamp.com>
2018-07-11 19:12:24 +0530

    Remove site-command tests. Update feature functions to only have ee test function.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -93,98 +93,12 @@ class FeatureContext implements Context
 		}
 	}
 
-	/**
-	 * @Then The :site db entry should be removed
-	 */
-	public function theDbEntryShouldBeRemoved($site)
-	{
-		$out = shell_exec("sudo bin/ee site list");
-		if (strpos($out, $site) !== false) {
-			throw new Exception("$site db entry not been removed!");
-		}
-
-	}
-
-	/**
-	 * @Then The :site webroot should be removed
-	 */
-	public function theWebrootShouldBeRemoved($site)
-	{
-		if (file_exists(getenv('HOME') . "/ee-sites/" . $site)) {
-			throw new Exception("Webroot has not been removed!");
-		}
-	}
-
-	/**
-	 * @Then Following containers of site :site should be removed:
-	 */
-	public function followingContainersOfSiteShouldBeRemoved($site, TableNode $table)
-	{
-		$containers = $table->getHash();
-		$site_name = implode(explode('.', $site));
-
-		foreach ($containers as $container) {
-
-			$sevice = $container['container'];
-			$container_name = $site_name . '_' . $sevice . '_1';
-
-			exec("docker inspect -f '{{.State.Running}}' $container_name > /dev/null 2>&1", $exec_out, $return);
-			if (!$return) {
-				throw new Exception("$container_name has not been removed!");
-			}
-		}
-	}
-
-	/**
-	 * @Then The site :site should have webroot
-	 */
-	public function theSiteShouldHaveWebroot($site)
-	{
-		if (!file_exists(getenv('HOME') . "/ee-sites/" . $site)) {
-			throw new Exception("Webroot has not been created!");
-		}
-	}
-
-	/**
-	 * @Then The site :site should have WordPress
-	 */
-	public function theSiteShouldHaveWordpress($site)
-	{
-		if (!file_exists(getenv('HOME') . "/ee-sites/" . $site . "/app/src/wp-config.php")) {
-			throw new Exception("WordPress data not found!");
-		}
-	}
-
-	/**
-	 * @Then Request on :site should contain following headers:
-	 */
-	public function requestOnShouldContainFollowingHeaders($site, TableNode $table)
-	{
-		$url = 'http://' . $site;
-
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, true);
-		curl_setopt($ch, CURLOPT_NOBODY, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$headers = curl_exec($ch);
-		curl_close($ch);
-
-		$rows = $table->getHash();
-
-		foreach ($rows as $row) {
-			if (strpos($headers, $row['header']) === false) {
-				throw new Exception("Unable to find " . $row['header'] . "\nActual output is : " . $headers);
-			}
-		}
-	}
-
+	
 	/**
 	 * @AfterFeature
 	 */
 	public static function cleanup(AfterFeatureScope $scope)
 	{
-		exec("sudo bin/ee site delete hello.test");
 		if(file_exists('ee.phar')) {
 			unlink('ee.phar');
 		}
