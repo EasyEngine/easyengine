d1d21239da92b188a6d07ccdce4f74867a02d619
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-07-26 20:37:33 +0530

    Implement list function
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- php/class-ee-site.php
+++ php/class-ee-site.php
@@ -17,7 +17,73 @@ abstract class EE_Site_Command {
 
 	public function __construct() {}
 
-	public function _list( $args, $assoc_args ) {}
+	/**
+	 * Lists the created websites.
+	 * abstract list
+	 *
+	 * [--enabled]
+	 * : List only enabled sites.
+	 *
+	 * [--disabled]
+	 * : List only disabled sites.
+	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format.
+	 * ---
+	 * default: table
+	 * options:
+	 *   - table
+	 *   - csv
+	 *   - yaml
+	 *   - json
+	 *   - count
+	 *   - text
+	 * ---
+	 *
+	 * @subcommand list
+	 */
+	public function _list( $args, $assoc_args ) {
+		\EE\Utils\delem_log( 'site list start' );
+		$format   = \EE\Utils\get_flag_value( $assoc_args, 'format' );
+		$enabled  = \EE\Utils\get_flag_value( $assoc_args, 'enabled' );
+		$disabled = \EE\Utils\get_flag_value( $assoc_args, 'disabled' );
+
+		$where = array();
+
+		if ( $enabled && ! $disabled ) {
+			$where['is_enabled'] = 1;
+		} elseif ( $disabled && ! $enabled ) {
+			$where['is_enabled'] = 0;
+		}
+
+		$sites = EE::db()::select( array( 'sitename', 'is_enabled' ), $where );
+
+		if ( ! $sites ) {
+			EE::error( 'No sites found!' );
+		}
+
+		if ( 'text' === $format ) {
+			foreach ( $sites as $site ) {
+				EE::log( $site['sitename'] );
+			}
+		} else {
+			$result = array_map(
+				function ( $site ) {
+					$site['site']   = $site['sitename'];
+					$site['status'] = $site['is_enabled'] ? 'enabled' : 'disabled';
+
+					return $site;
+				}, $sites
+			);
+
+			$formatter = new \EE\Formatter( $assoc_args, [ 'site', 'status' ] );
+
+			$formatter->display_items( $result );
+		}
+
+		\EE\Utils\delem_log( 'site list end' );
+	}
+
 
 	public function delete( $args, $assoc_args ) {}
 
