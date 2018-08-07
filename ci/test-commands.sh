c387605dad285b4c61c4028a1223f43ce6ad92d2
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-08-07 20:16:41 +0530

    Add certificates
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- ci/test-commands.sh
+++ ci/test-commands.sh
@@ -2,6 +2,9 @@
 
 # called by Travis CI
 
+# Add certificates
+./vendor/easyengine/site-command/ci/add-test-certs.sh
+
 for repo in "$(find vendor/easyengine -type d -name 'features')"; do
 	rsync -a --delete $repo/ features
 	sudo ./vendor/bin/behat 
