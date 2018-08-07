54ba8ae35b762c1a0bb0d0147e0f36690c75ace0
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-08-07 20:27:05 +0530

    Update display msgs
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- ci/test-commands.sh
+++ ci/test-commands.sh
@@ -3,9 +3,10 @@
 # called by Travis CI
 
 # Add certificates
-./vendor/easyengine/site-command/ci/add-test-certs.sh
+./vendor/easyengine/site-command/ci/add-test-certs.sh > /dev/null
 
 for repo in "$(find vendor/easyengine -type d -name 'features')"; do
-	rsync -a --delete $repo/ features
+	rsync -a --delete $repo/ features > /dev/null
+	echo "Running tests for $repo"
 	sudo ./vendor/bin/behat 
 done
