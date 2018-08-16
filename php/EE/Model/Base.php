14b24b916af04c08c534b53a6559f760f5eaf433
Kirtan Gajjar <kirtangajjar95@gmail.com>
2018-08-16 14:47:19 +0530

    Fix phpcs errors
    Signed-off-by: Kirtan Gajjar <kirtangajjar95@gmail.com>

--- php/EE/Model/Base.php
+++ php/EE/Model/Base.php
@@ -34,89 +34,9 @@ abstract class Base {
 	}
 
 	/**
-	 * In model, every value is get in fields array.
-	 * We populate it either during constructor or during find() method call
-	 *
-	 * This gives us benefit that models do not have to define properties in class
-	 * They are automatically defined when fetched from database!
-	 *
-	 * @param string $name Name of property to get
-	 *
-	 * @throws \Exception
-	 *
-	 * @return mixed Value of property
-	 */
-	public function __get( string $name ) {
-		if ( array_key_exists( $name, $this->fields ) ) {
-			return $this->fields[ $name ];
-		}
-
-		throw new \Exception( "Unable to find variable: $name" );
-	}
-
-	/**
-	 * In model, every value is set in fields array.
-	 *
-	 * This gives us benefit that models do not have to define the logic of saving them in database.
-	 * While saving models, we use the $fields array to save it in database
-	 *
-	 *
-	 * @param string $name  Name of property to set
-	 * @param mixed  $value Value of property to set
-	 */
-	public function __set( string $name, $value ) {
-		$this->fields[ $name ] = $value;
-	}
-
-	/**
-	 * Overriding isset for correct behaviour while using isset on model objects
-	 *
-	 * @param string|int $name Name of property to check
-	 *
-	 * @return bool
-	 */
-	public function __isset( $name ) {
-		return isset( $this->fields[ $name ] );
-	}
-
-	/**
-	 * Removes a property from model
-	 * It's done by removing it from $fields array
-	 *
-	 * @param string $name  Name of property to unset
-	 */
-	public function __unset( $name ) {
-		unset( $this->fields[ $name ] );
-	}
-
-	/**
-	 * Returns single model fetched by primary key
-	 *
-	 * @param string $value value to find
-	 * @param string $column Column to find in. Defaults to primary key
-	 *
-	 * @throws \Exception
-	 *
-	 * @return static
-	 */
-	public static function find( string $value, string $column = null ) {
-		$primary_key_column = $column ?? static::$primary_key;
-		$model = EE::db()
-			->table( static::$table )
-			->where( $primary_key_column, $value )
-			->first();
-
-		if( $model === false ) {
-			return false;
-		}
-
-		return static::single_array_to_model( $model );
-	}
-
-	/**
 	 * Throws exception if model is not found
 	 *
-	 * @param string $value value to find
+	 * @param string $value  value to find
 	 * @param string $column Column to find in. Defaults to primary key
 	 *
 	 * @throws \Exception
@@ -129,13 +49,49 @@ abstract class Base {
 		if ( ! $model ) {
 			throw new \Exception( sprintf( 'Unable to find %s : with primary key: %s and value: %s', __CLASS__, static::$primary_key, $value ) );
 		}
+
 		return $model;
+	}
+
+	/**
+	 * Returns single model fetched by primary key
+	 *
+	 * @param string $value  value to find
+	 * @param string $column Column to find in. Defaults to primary key
+	 *
+	 * @throws \Exception
+	 *
+	 * @return static
+	 */
+	public static function find( string $value, string $column = null ) {
+		$primary_key_column = $column ?? static::$primary_key;
+		$model              = EE::db()
+			->table( static::$table )
+			->where( $primary_key_column, $value )
+			->first();
+
+		if ( false === $model ) {
+			return false;
+		}
+
+		return static::single_array_to_model( $model );
+	}
+
+	/**
+	 * Converts single row fetched from database into model
+	 *
+	 * @param array $arr Associative array representing a row from database
+	 *
+	 * @return mixed
+	 */
+	protected static function single_array_to_model( array $arr ) {
+		return new static( $arr );
 	}
 
 	/**
 	 * Exits with error if model is not found
 	 *
-	 * @param string $value value to find
+	 * @param string $value  value to find
 	 * @param string $column Column to find in. Defaults to primary key
 	 *
 	 * @throws \Exception
@@ -148,6 +104,7 @@ abstract class Base {
 		if ( ! $model ) {
 			EE::error( sprintf( 'Unable to find %s : with primary key: %s and value: %s', __CLASS__, static::$primary_key, $value ) );
 		}
+
 		return $model;
 	}
 
@@ -170,6 +127,23 @@ abstract class Base {
 	}
 
 	/**
+	 * Converts an array of result from database to models
+	 *
+	 * @param array $arr Array of results from database
+	 *
+	 * @return array
+	 */
+	protected static function many_array_to_model( array $arr ) {
+		$result = [];
+
+		foreach ( $arr as $model ) {
+			$result[] = static::single_array_to_model( $model );
+		}
+
+		return $result;
+	}
+
+	/**
 	 * Creates new entity in DB
 	 *
 	 * @param array $columns Columns and values to insert
@@ -183,6 +157,81 @@ abstract class Base {
 	}
 
 	/**
+	 * Returns all model with condition
+	 *
+	 * @param string|array $column Column to search in
+	 * @param string|int   $value  Value to match
+	 *
+	 * @throws \Exception
+	 *
+	 * @return array
+	 */
+	public static function where( $column, $value ) {
+		return static::many_array_to_model(
+			EE::db()
+				->table( static::$table )
+				->where( $column, $value )
+				->all()
+		);
+	}
+
+	/**
+	 * In model, every value is get in fields array.
+	 * We populate it either during constructor or during find() method call
+	 *
+	 * This gives us benefit that models do not have to define properties in class
+	 * They are automatically defined when fetched from database!
+	 *
+	 * @param string $name Name of property to get
+	 *
+	 * @throws \Exception
+	 *
+	 * @return mixed Value of property
+	 */
+	public function __get( string $name ) {
+		if ( array_key_exists( $name, $this->fields ) ) {
+			return $this->fields[$name];
+		}
+
+		throw new \Exception( "Unable to find variable: $name" );
+	}
+
+	/**
+	 * In model, every value is set in fields array.
+	 *
+	 * This gives us benefit that models do not have to define the logic of saving them in database.
+	 * While saving models, we use the $fields array to save it in database
+	 *
+	 *
+	 * @param string $name  Name of property to set
+	 * @param mixed  $value Value of property to set
+	 */
+	public function __set( string $name, $value ) {
+		$this->fields[$name] = $value;
+	}
+
+	/**
+	 * Overriding isset for correct behaviour while using isset on model objects
+	 *
+	 * @param string|int $name Name of property to check
+	 *
+	 * @return bool
+	 */
+	public function __isset( $name ) {
+		return isset( $this->fields[$name] );
+	}
+
+	/**
+	 * Removes a property from model
+	 * It's done by removing it from $fields array
+	 *
+	 * @param string $name Name of property to unset
+	 */
+	public function __unset( $name ) {
+		unset( $this->fields[$name] );
+	}
+
+	/**
 	 * Saves current model into database
 	 *
 	 * @throws \Exception
@@ -190,13 +239,15 @@ abstract class Base {
 	 * @return bool Model saved successfully
 	 */
 	public function save() {
-		$fields = array_merge( $this->fields, [
-			'modified_on' => date( 'Y-m-d H:i:s' ),
-		] );
+		$fields = array_merge(
+			$this->fields, [
+				'modified_on' => date( 'Y-m-d H:i:s' ),
+			]
+		);
 
 		return EE::db()
 			->table( static::$table )
-			->where( static::$primary_key, $this[ static::$primary_key ] )
+			->where( static::$primary_key, $this[static::$primary_key] )
 			->update( $fields );
 	}
 
@@ -226,52 +277,5 @@ abstract class Base {
 			->table( static::$table )
 			->where( static::$primary_key, $this->id )
 			->update( $columns );
-	}
-
-	/**
-	 * Returns all model with condition
-	 *
-	 * @param string|array $column Column to search in
-	 * @param string|int   $value  Value to match
-	 *
-	 * @throws \Exception
-	 *
-	 * @return array
-	 */
-	public static function where( $column, $value ) {
-		return static::many_array_to_model(
-			EE::db()
-				->table( static::$table )
-				->where( $column, $value )
-				->all()
-		);
-	}
-
-	/**
-	 * Converts single row fetched from database into model
-	 *
-	 * @param array $arr Associative array representing a row from database
-	 *
-	 * @return mixed
-	 */
-	protected static function single_array_to_model( array $arr ) {
-		return new static( $arr );
-	}
-
-	/**
-	 * Converts an array of result from database to models
-	 *
-	 * @param array $arr Array of results from database
-	 *
-	 * @return array
-	 */
-	protected static function many_array_to_model( array $arr ) {
-		$result = [];
-
-		foreach ( $arr as $model ) {
-			$result[] = static::single_array_to_model( $model );
-		}
-
-		return $result;
 	}
 }
