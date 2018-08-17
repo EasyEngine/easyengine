09ce253dd53b2e40232143c6d49fe4f0dc702554
Mriyam Tamuli <mbtamuli@gmail.com>
2018-08-17 18:00:26 +0530

    Fix erroneous variable name
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- ci/test-commands.sh
+++ ci/test-commands.sh
@@ -6,20 +6,15 @@
 ./vendor/easyengine/site-command/ci/add-test-certs.sh > /dev/null
 
 if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
-	commit_msg="$(git --no-pager log -2 --pretty=%B)"
+	commit_message="$(git --no-pager log -2 --pretty=%B)"
 else
-	commit_msg=$TRAVIS_COMMIT_MESSAGE
+	commit_message=$TRAVIS_COMMIT_MESSAGE
 fi
-
-echo "Commit msg: $commit_msg"
 
 echo "Starting test suite for other commands"
 
-if [[ $commit_msg = *"[BREAKING CHANGES]"* ]]; then
-
-	echo "[BREAKING CHANGES] found."
+if [[ $commit_message = *"[BREAKING CHANGES]"* ]]; then
 	pattern='\<REPO\>.*\b'
-
 	declare -a repos
 
 	for word in $commit_message; do
@@ -30,22 +25,20 @@ if [[ $commit_msg = *"[BREAKING CHANGES]"* ]]; then
 	done
 
 	pushd vendor/easyengine
-
-	for repo in ${repos[@]}; do
-		owner_name_unclean=${repo%%\/*}
-		owner_name=${owner_name_unclean#*\=}
-		repo_with_branch=${repo#*\/}
-		repo_name=${repo_with_branch%\:*}
-		repo_branch=${repo_with_branch#*\:}
-		sudo rm -r "$repo_name"
-		git clone https://github.com/"$owner_name"/"$repo_name".git -b "$repo_branch"
-		pushd "$repo_name"
-		echo "Updated to easyengine/$repo_name to $owner_name/$repo_name"
-		git remote -v
-		git branch
-		popd
-	done
-
+		for repo in ${repos[@]}; do
+			owner_name_unclean=${repo%%\/*}
+			owner_name=${owner_name_unclean#*\=}
+			repo_with_branch=${repo#*\/}
+			repo_name=${repo_with_branch%\:*}
+			repo_branch=${repo_with_branch#*\:}
+			sudo rm -r "$repo_name"
+			git clone https://github.com/"$owner_name"/"$repo_name".git -b "$repo_branch"
+			pushd "$repo_name"
+				echo "Updated to easyengine/$repo_name to $owner_name/$repo_name"
+				git remote -v
+				git branch
+			popd
+		done
 	popd
 fi
 
@@ -53,5 +46,5 @@ find vendor/easyengine -type d -name 'features' | while read repo; do
 	repo_name=$(echo $repo | awk -F  "/" '{print $3}');
 	rsync -a --delete $repo/ features > /dev/null
 	echo "Running tests for $repo_name"
-	sudo ./vendor/bin/behat	
+	sudo ./vendor/bin/behat
 done
