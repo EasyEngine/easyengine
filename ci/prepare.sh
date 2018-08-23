cdb78c09af84f23f2aee2457062a4585074253a2
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-08-23 15:23:14 +0530

    Update images to nightly for non-deploy branches
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -2,11 +2,12 @@
 
 # called by Travis CI
 
-if [[ "$TRAVIS_BRANCH" == "develop-v4" ]]; then
+if [[ "$TRAVIS_BRANCH" != $DEPLOY_BRANCH ]]; then
     version=$(head -n 1 VERSION)
     version="$(echo $version | xargs)"
     version+="-nightly"
     echo $version > VERSION
+    sed -i 's/\:\ \"\(.*\)\"/\:\ \"\1-nightly\"/g' img-versions.json
 fi
 
 php -dphar.readonly=0 ./utils/make-phar.php easyengine.phar --quiet
