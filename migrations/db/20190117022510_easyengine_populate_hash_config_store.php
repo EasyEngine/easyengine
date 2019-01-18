7f5c61a31002f47f7e7f42f8f6f1014aeaa5134d
Thrijith Thankachan <thrijith13@gmail.com>
2019-01-18 23:45:48 +0530

    Move necessary functions to ConfigHash model for usage in site and service creation

--- migrations/db/20190117022510_easyengine_populate_hash_config_store.php
+++ migrations/db/20190117022510_easyengine_populate_hash_config_store.php
@@ -4,9 +4,6 @@ namespace EE\Migration;
 use EE;
 use EE\Model\Site;
 use EE\Model\ConfigHash;
-use EE\Utils;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
 
 class PopulateHashConfigStore extends Base {
 
@@ -55,10 +52,10 @@ class PopulateHashConfigStore extends Base {
 			foreach ( $site_conf_paths as $site_conf_path ) {
 
 				// get all files in given path.
-				$files = $this->get_files_in_path( $site_conf_path );
+				$files = ConfigHash::get_files_in_path( $site_conf_path );
 
 				// insert hash record for found files.
-				$this->insert_hash_data( $files, $site->site_url );
+				ConfigHash::insert_hash_data( $files, $site->site_url );
 			}
 		}
 
@@ -70,10 +67,10 @@ class PopulateHashConfigStore extends Base {
 			$service_path = EE_ROOT_DIR . DIRECTORY_SEPARATOR . 'services' . DIRECTORY_SEPARATOR . $service;
 
 			// get all files in given path.
-			$files = $this->get_files_in_path( $service_path );
+			$files = ConfigHash::get_files_in_path( $service_path );
 
 			// insert hash record for found files.
-			$this->insert_hash_data( $files, $service );
+			ConfigHash::insert_hash_data( $files, $service );
 		}
 	}
 
@@ -93,57 +90,4 @@ class PopulateHashConfigStore extends Base {
 		}
 	}
 
-	/**
-	 * Get all files in the provided directory.
-	 *
-	 * @param string $path Path to directory.
-	 *
-	 * @return array
-	 */
-	private function get_files_in_path( $path ) {
-
-		// Allowed file extensions which should be hashed.
-		$allowed_ext = [ 'cnf', 'conf', 'ini' ];
-
-		$recursive_iterator = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $path ) );
-
-		$files = array();
-
-		// get all files in the config directory.
-		foreach ( $recursive_iterator as $file ) {
-
-			if ( ! $file->isDir() ) {
-				$file_path = $file->getPathname();
-				$file_info = pathinfo( $file_path );
-
-				// check if a file has an extension.
-				$extension = ( ! empty( $file_info['extension'] ) ) ? $file_info['extension'] : '';
-
-				// store files which have valid extensions.
-				if ( in_array( $extension, $allowed_ext, true ) ) {
-					$files[] = $file_path;
-				}
-			}
-		}
-
-		return $files;
-	}
-
-	/**
-	 * @param array  $files       Array of files for hash creation.
-	 * @param string $config_root Root path where the config is stored.
-	 *
-	 * @throws \Exception
-	 */
-	private function insert_hash_data( $files, $config_root ) {
-
-		// loop through all the files and create it hash record if not exists.
-		foreach ( $files as $filepath ) {
-
-			if ( false === ConfigHash::get_config_info( $filepath ) ) {
-				Utils\create_config_file_hash( $filepath, $config_root );
-			}
-		}
-
-	}
 }
--- php/EE/Model/ConfigHash.php
+++ php/EE/Model/ConfigHash.php
@@ -2,6 +2,10 @@
 
 namespace EE\Model;
 
+use EE\Utils;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
 /**
  * ConfigHash model class.
  */
@@ -38,9 +42,63 @@ class ConfigHash extends Base {
 	public static function get_config_info( $config_path ) {
 
 		return \EE::db()
-				->table( 'hash_config_store' )
+				->table( static::$table )
 				->where( 'conf_file_path', '=', $config_path )
 				->first();
+
+	}
+
+	/**
+	 * Get all files in the provided directory.
+	 *
+	 * @param string $path Path to directory.
+	 *
+	 * @return array
+	 */
+	public static function get_files_in_path( $path ) {
+
+		// Allowed file extensions which should be hashed.
+		$allowed_ext = [ 'cnf', 'conf', 'ini' ];
+
+		$recursive_iterator = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $path ) );
+
+		$files = array();
+
+		// get all files in the config directory.
+		foreach ( $recursive_iterator as $file ) {
+
+			if ( ! $file->isDir() ) {
+				$file_path = $file->getPathname();
+				$file_info = pathinfo( $file_path );
+
+				// check if a file has an extension.
+				$extension = ( ! empty( $file_info['extension'] ) ) ? $file_info['extension'] : '';
+
+				// store files which have valid extensions.
+				if ( in_array( $extension, $allowed_ext, true ) ) {
+					$files[] = $file_path;
+				}
+			}
+		}
+
+		return $files;
+	}
+
+	/**
+	 * @param array  $files       Array of files for hash creation.
+	 * @param string $config_root Root path where the config is stored.
+	 *
+	 * @throws \Exception
+	 */
+	public static function insert_hash_data( $files, $config_root ) {
+
+		// loop through all the files and create it hash record if not exists.
+		foreach ( $files as $filepath ) {
+
+			if ( false === ConfigHash::get_config_info( $filepath ) ) {
+				Utils\create_config_file_hash( $filepath, $config_root );
+			}
+		}
 
 	}
 
