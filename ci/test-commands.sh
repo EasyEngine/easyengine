089ab0b4938603489cd3f66608a338dbcdee8722
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2019-05-06 17:58:33 +0530

    Update test running and travis config
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- ci/test-commands.sh
+++ ci/test-commands.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-
+set -ex
 # called by Travis CI
 
-# Add certificates
-./vendor/easyengine/site-command/ci/add-test-certs.sh > /dev/null
+#repos=$(find vendor/easyengine -type d -name 'features')
+sub_commands=(
+	vendor/easyengine/site-command/features
+	vendor/easyengine/site-type-php/features
+	vendor/easyengine/site-type-wp/features
+)
 
-for repo in "$(find vendor/easyengine -type d -name 'features')"; do
-	rsync -a --delete $repo/ features > /dev/null
-	echo "Running tests for $repo"
-	sudo ./vendor/bin/behat 
+for command in "${sub_commands[@]}"; do
+	IFS='/' read -r -a array <<< "$command"
+	rm -rf features/*
+	rsync -avP --delete $command/ features/
+	for file in features/*.feature; do mv "$file" "${file%.feature}_${array[2]}.feature"; done
+	composer du
+	echo "Running tests for $command"
+	export COMPOSE_INTERACTIVE_NO_CLI=1
+	sudo ./vendor/bin/behat
 done
