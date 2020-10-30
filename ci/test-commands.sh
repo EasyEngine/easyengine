5c1942283df4c5ab94f9909f388fff8f8ba28c29
Riddhesh Sanghvi <riddhesh237@gmail.com>
2020-10-30 17:13:12 +0530

    Add debugging to testing script
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- ci/test-commands.sh
+++ ci/test-commands.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # called by Travis CI
 
+set -x
+
 #repos=$(find vendor/easyengine -type d -name 'features')
 sub_commands=(
 	vendor/easyengine/site-command/features
 	vendor/easyengine/site-type-php/features
 	vendor/easyengine/site-type-wp/features
 )
+
+export COMPOSE_INTERACTIVE_NO_CLI=1
 
 for command in "${sub_commands[@]}"; do
 	IFS='/' read -r -a array <<< "$command"
@@ -15,6 +19,5 @@ for command in "${sub_commands[@]}"; do
 	for file in features/*.feature; do mv "$file" "${file%.feature}_${array[2]}.feature"; done
 	composer du
 	echo "Running tests for $command"
-	export COMPOSE_INTERACTIVE_NO_CLI=1
 	sudo ./vendor/bin/behat
 done
