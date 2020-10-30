1d754895cdb8ea0b519571f62309b77ca4858ade
Riddhesh Sanghvi <riddhesh237@gmail.com>
2020-10-30 17:13:58 +0530

    Remove composer du
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- ci/test-commands.sh
+++ ci/test-commands.sh
@@ -10,14 +10,11 @@ sub_commands=(
 	vendor/easyengine/site-type-wp/features
 )
 
-export COMPOSE_INTERACTIVE_NO_CLI=1
-
 for command in "${sub_commands[@]}"; do
 	IFS='/' read -r -a array <<< "$command"
 	rm -rf features/*
 	rsync -av --delete $command/ features/ > /dev/null
 	for file in features/*.feature; do mv "$file" "${file%.feature}_${array[2]}.feature"; done
-	composer du
 	echo "Running tests for $command"
 	sudo ./vendor/bin/behat
 done
