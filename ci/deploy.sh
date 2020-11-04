fbe043238cdfce68493d52e397aea15e49d43f26
Riddhesh Sanghvi <riddhesh237@gmail.com>
2020-11-04 16:22:10 +0530

    Re-create phar without dev dependencies
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -30,6 +30,16 @@ set -x
 # add github's public key
 echo "|1|qPmmP7LVZ7Qbpk7AylmkfR0FApQ=|WUy1WS3F4qcr3R5Sc728778goPw= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts
 
+# Remove dev dependenices needed for testing
+if [[ "$TRAVIS_BRANCH" = "master" ]]; then
+	composer install --prefer-dist --no-suggest --no-progress --no-interaction --no-dev
+else
+	composer update --prefer-dist --no-suggest --no-progress --no-interaction --no-dev
+fi
+
+# Re create phar
+bash "$TRAVIS_BUILD_DIR/ci/prepare.sh"
+
 git clone git@github.com:easyengine/easyengine-builds.git
 
 git config --global user.name "rtBot"
