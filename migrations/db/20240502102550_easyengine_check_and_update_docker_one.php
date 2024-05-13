8f9eda041f4bacbc5ebeef20ad446cc7617958f2
Riddhesh Sanghvi <riddhesh237@gmail.com>
2024-05-13 16:03:55 +0530

    Add migration for docker update
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- migrations/db/20240502102550_easyengine_check_and_update_docker_one.php
+++ migrations/db/20240502102550_easyengine_check_and_update_docker_one.php
@@ -4,6 +4,7 @@ namespace EE\Migration;
 
 use EE;
 use EE\Migration\Base;
+use Symfony\Component\Filesystem\Filesystem;
 
 class CheckAndUpdateDockerOne extends Base {
 
@@ -15,7 +16,7 @@ class CheckAndUpdateDockerOne extends Base {
 	public function up() {
 
 		EE::log( 'Checking Docker version.' );
-		$docker_version = EE::launch( 'docker version --format "{{.Server.Version}}"' )->stdout;
+		$docker_version = trim( EE::launch( 'docker version --format "{{.Server.Version}}"' )->stdout );
 
 		if ( version_compare( $docker_version, '20.10.10', '<' ) ) {
 			EE::warning( 'Docker version should be 20.10.10 or above.' );
@@ -33,10 +34,30 @@ class CheckAndUpdateDockerOne extends Base {
 			}
 		}
 
+		EE::log( 'Checking docker-compose version' );
+		$docker_compose_version     = trim( EE::launch( 'docker-compose version --short' )->stdout );
+		$docker_compose_path        = EE::launch( 'command -v docker-compose' )->stdout;
+		$docker_compose_path        = trim( $docker_compose_path );
+		$docker_compose_backup_path = EE_BACKUP_DIR . '/docker-compose.backup';
+		$fs                         = new Filesystem();
+		if ( ! $fs->exists( EE_BACKUP_DIR ) ) {
+			$fs->mkdir( EE_BACKUP_DIR );
+		}
+		$fs->copy( $docker_compose_path, $docker_compose_backup_path );
+
+		if ( version_compare( '1.29.2', $docker_compose_version, '>' ) ) {
+			EE::exec( "curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o $docker_compose_path && chmod +x $docker_compose_path" );
+		}
+
 		// Check the version again post update.
-		$docker_version = EE::launch( 'docker version --format "{{.Server.Version}}"' )->stdout;
+		$docker_version = trim( EE::launch( 'docker version --format "{{.Server.Version}}"' )->stdout );
 		if ( version_compare( $docker_version, '20.10.10', '<' ) ) {
 			EE::error( 'Docker version should be 20.10.10 or above. Please update Docker and try again.' );
+		}
+
+		$docker_compose_version = trim( EE::launch( 'docker-compose version --short' )->stdout );
+		if ( version_compare( '1.29.2', $docker_compose_version, '!=' ) ) {
+			EE::error( 'Docker-compose version should be 1.29.2. Please update Docker-compose and try again.' );
 		}
 	}
 
