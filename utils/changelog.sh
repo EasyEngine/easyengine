73be7e9cb7b63e0546cff4c31ba2b2dae9616e34
Riddhesh Sanghvi <riddhesh237@gmail.com>
2025-09-03 18:25:33 +0530

    chore(changelog): update changelog script for accuracy

--- utils/changelog.sh
+++ utils/changelog.sh
@@ -1,21 +1,41 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo "## What's Changed" > changelog.txt
+echo -e "## What's Changed\n" > changelog.txt
 
+# Get the creation date of the most recent release
 createdAt=$(gh api graphql -F owner='EasyEngine' -F name='easyengine' -f query='
   query {
     repository(owner: "EasyEngine", name: "easyengine") {
-      releases(first: 1, orderBy: { field: CREATED_AT, direction: DESC }) {
+      releases(first: 1, orderBy: {field: CREATED_AT, direction: DESC}) {
         nodes { tagName, createdAt }
       }
     }
   }
 ' | jq -r '.data.repository.releases.nodes[0].createdAt')
 
+# Also get the tag name for verification
+tagName=$(gh api graphql -F owner='EasyEngine' -F name='easyengine' -f query='
+  query {
+    repository(owner: "EasyEngine", name: "easyengine") {
+      releases(first: 1, orderBy: {field: CREATED_AT, direction: DESC}) {
+        nodes { tagName, createdAt }
+      }
+    }
+  }
+' | jq -r '.data.repository.releases.nodes[0].tagName')
+
+echo "Last release: $tagName ($createdAt)"
+
+# Search for merged PRs since the last release with proper pagination
+# Using merged:>$createdAt instead of updated:>$createdAt
 gh api graphql --paginate -f query="
-query {
-  search(query: \"org:Easyengine updated:>$createdAt state:closed is:pr\", type:ISSUE, first: 100) {
+query(\$endCursor: String) {
+  search(query: \"org:EasyEngine merged:>$createdAt is:pr is:merged\", type: ISSUE, first: 100, after: \$endCursor) {
     repositoryCount
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
     edges {
       node {
         ... on PullRequest {
@@ -25,10 +45,13 @@ query {
           author {
             login
           }
-          updatedAt
+          mergedAt
+          repository {
+            name
+          }
         }
       }
     }
   }
 }
-" --template '{{range .data.search.edges}}{{"* "}}{{.node.title}}{{" "}}{{.node.permalink}}{{" "}}{{.node.state}}{{" @"}}{{.node.author.login}}{{"\n"}}{{end}}' | sed '/CLOSED/d' | sed 's/ MERGED//g' >> changelog.txt
+" --template '{{range .data.search.edges}}{{"* "}}{{.node.title}} {{.node.permalink}}{{" @"}}{{.node.author.login}}{{"\n"}}{{end}}' >> changelog.txt
